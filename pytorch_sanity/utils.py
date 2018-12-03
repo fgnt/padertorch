32462aebdd221f46b6c1778b3653916e55d270e2
jensheit <heitkaemper@nt.upb.de>
2018-12-03 16:49:57 +0100

    added options to collate_fn and allows nested dicts

--- pytorch_sanity/utils.py
+++ pytorch_sanity/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 def to_list(x, length=None):
     if not isinstance(x, list):
@@ -45,35 +46,93 @@ def pad_tensor(vec, pad, axis):
     return np.concatenate([vec, np.zeros(pad_size)], axis=axis)
 
 
-def pad_batch(batch):
+def pad_batch(batch, to_torch=True, device='cpu', sort_by_length=False):
 
     if isinstance(batch[0], np.ndarray):
         if len(batch[0].shape) > 0:
+
             dims = np.array([[idx for idx in array.shape] for array in batch]).T
             axis = [idx for idx, dim in enumerate(dims) if
                     not all(dim == dim[::-1])]
-            assert len(axis) == 1, f'only one axis is allowed to differ,' \
-                                   f' axis={axis} and dims={dims}'
-            axis = axis[0]
-            sorted_batch = sorted(batch, key=lambda x: x.shape[axis],
-                                  reverse=True)
-            pad = sorted_batch[0].shape[axis]
-            return np.stack([pad_tensor(vec, pad, axis)
-                         for vec in sorted_batch], axis=0)
+
+            assert len(axis) in [0,1], f'only one axis is allowed to differ,' \
+                                       f' axis={axis} and dims={dims}'
+            if len(axis) == 1:
+                axis = axis[0]
+                if sort_by_length:
+                    batch = sorted(batch, key=lambda x: x.shape[axis],
+                                          reverse=True)
+                pad = max(dims[axis])
+                array =  np.stack([pad_tensor(vec, pad, axis)
+                                   for vec in batch], axis=0)
+            else:
+                array = np.stack(batch, axis=0)
+            if to_torch:
+                return torch.from_numpy(array).to(device)
+            else:
+                return array
         else:
+            # sort num_samples / num_frames to fit the sorted batches
+            if sort_by_length:
+                return np.array(sorted(batch, reverse=True))
+            else:
+                return np.array(batch)
+    elif isinstance(batch[0], int):
+        # sort num_samples / num_frames to fit the sorted batches
+        if sort_by_length:
             return np.array(sorted(batch, reverse=True))
+        else:
+            return np.array(batch)
     else:
         return batch
 
 
 
-def collate_fn(batch):
-    # only works with shallow dict structure
-    merged_batch = {}
+def collate_fn(batch, padding=True, padding_fn=pad_batch, padding_keys=None,
+               to_torch=True, device='cpu', sort_by_length=False):
+    # assumes batch to be a list of dicts
+    # ToDo: do we automaticaly sort by sequence length?
+    assert not to_torch ^ (padding and to_torch)
+
+    def nested_batching(value, key, nested_batch):
+        # recursively nesting the batch
+        if isinstance(value, dict):
+            if not key in nested_batch:
+                nested_batch[key] = dict()
+            return {k: nested_batching(v, k, nested_batch[key])
+                    for k, v in value.items()}
+        else:
+            if not key in nested_batch:
+                nested_batch[key] = []
+            nested_batch[key].append(value)
+        return nested_batch[key]
+
+    nested_batch = {}
     for elem in batch:
-        if isinstance(elem, dict):
-            for key, value in elem.items():
-                if key not in merged_batch:
-                    merged_batch[key] = []
-                merged_batch[key].append(value)
-    return {key: pad_batch(vec) for key, vec in merged_batch.items()}
+        assert isinstance(elem, dict)
+        nested_batch = {key: nested_batching(value, key, nested_batch)
+                        for key, value in elem.items()}
+
+    if padding:
+        if padding_keys is None:
+            padding_keys = nested_batch.keys()
+        else:
+            assert len(padding_keys) > 0, 'Empty padding key list was ' \
+                                          'provided default should be None'
+
+        def nested_padding(value, key):
+            if isinstance(value, dict):
+                return {k: nested_padding(v, k) for k, v in value.items()}
+            else:
+                if key in padding_keys:
+                    return padding_fn(value, to_torch=to_torch, device=device,
+                                      sort_by_length=sort_by_length)
+                else:
+                    return value
+        return {key: nested_padding(value, key) for key, value in nested_batch.items()}
+    else:
+        assert padding_keys is None or len(padding_keys) == 0, (
+            'Padding keys have to be None or empty if padding is set to False, '
+            'but they are:', padding_keys
+            )
+        return nested_batch
