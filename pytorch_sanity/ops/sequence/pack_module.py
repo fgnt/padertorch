c1d110a81688ff72ea9ef03c6e0d5da98435761a
jensheit <heitkaemper@nt.upb.de>
2018-12-18 15:55:37 +0100

    fixes to pack_module

--- pytorch_sanity/ops/sequence/pack_module.py
+++ pytorch_sanity/ops/sequence/pack_module.py
@@ -1,6 +1,8 @@
 import torch
 from torch.nn.utils.rnn import pack_sequence as pack_sequence_
 from torch.nn.utils.rnn import pad_packed_sequence as pad_packed_sequence_
+from torch.nn.utils.rnn import pack_padded_sequence as pack_padded_sequence_
+from torch.nn.utils.rnn import pad_sequence as pad_sequence_
 from torch.nn.utils.rnn import PackedSequence
 
 
@@ -15,24 +17,24 @@ __all__ = [
 
 
 def pack_sequence(sequences: list) -> PackedSequence:
-    pass
-
+    return pack_padded_sequence_(pad_sequence(sequences),
+                                 [v.size(0) for v in sequences])
 
 def unpack_sequence(packed_sequence: PackedSequence) -> list:
-    pass
+    return unpad_sequence(*pad_packed_sequence(packed_sequence))
 
 
-def pad_sequence(sequences: list) -> (torch.Tensor, list):
-    pass
+def pad_sequence(sequences: list) -> (torch.Tensor, torch.LongTensor):
+    return pad_sequence_(sequences)
 
 
 def unpad_sequence(padded_sequence: torch.Tensor, lengths: list):
-    pass
+    return [padded_sequence[:l, b, ...] for b, l in enumerate(lengths)]
 
 
-def pad_packed_sequence(packed_sequence: PackedSequence) -> (torch.Tensor, list):
-    pass
+def pad_packed_sequence(packed_sequence: PackedSequence) -> (torch.Tensor, torch.LongTensor):
+    return pad_packed_sequence_(packed_sequence)
 
 
-def pack_padded_sequence(padded_sequence: torch.Tensor, lengths: list) -> PackedSequence:
-    pass
+def pack_padded_sequence(padded_sequence: torch.Tensor, lengths: torch.LongTensor) -> PackedSequence:
+    return pack_padded_sequence_(padded_sequence, lengths)
