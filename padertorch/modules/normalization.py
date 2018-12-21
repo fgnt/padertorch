small fixes to normalization

padertorch/modules/normalization.py
@@ -9,6 +9,9 @@ class Normalization(pt.Module):
     :param order: Order of the normalization. Can be l1 or l2. For l1, the
         tensor is normalized by \\(\\frac{\\gamma(x-\\mu)}{}\\)
     :param statistics_axis: axis along which to compute the statics.
+    :param independent_axis: int, tuple of ints specifying independent
+        dimensions. Determines shape of the kernel for
+        shift and scale operations.
     :param norm_epsilon: prevents division by 0
     :param keep_dims: keep the number of dimensions
     :param affine: a boolean value that when set to ``True``, this module has
@@ -17,8 +20,7 @@ class Normalization(pt.Module):
     :return: tuple of lp-statistics
     """
     def __init__(self, num_features=None, order='l2', statistics_axis=0,
-                 independent_axis=-1, norm_epsilon=1e-5,
-                 affine=True):
+                 independent_axis=-1, norm_epsilon=1e-5, affine=True):
         assert num_features is not None
         super().__init__()
         self.num_features = num_features
@@ -55,14 +57,14 @@ class Normalization(pt.Module):
             norm = sequence_reduction(
                 torch.mean,
                 torch.abs(tensor),
-                axis = self.statistics_axis,
+                axis=self.statistics_axis,
                 keepdims=True
             )
         elif self.order in ['l2', 'mean']:
             mean = sequence_reduction(
                 torch.mean,
                 tensor,
-                dim=self.statistics_axis,
+                axis=self.statistics_axis,
                 keepdims=True
             )
             norm = sequence_reduction(