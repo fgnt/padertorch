135827ebf79c68755431e304315212567d9230f2
jensheit <heitkaemper@nt.upb.de>
2018-12-21 12:11:23 +0100

    only adds not in torch implemented functions and imports the others

--- padertorch/ops/sequence/pack_module.py
+++ padertorch/ops/sequence/pack_module.py
@@ -5,11 +5,11 @@ padded
 list of tensor
 '''
 import torch
-from torch.nn.utils.rnn import pad_packed_sequence as pad_packed_sequence_
-from torch.nn.utils.rnn import pack_padded_sequence as pack_padded_sequence_
-from torch.nn.utils.rnn import pad_sequence as pad_sequence_
 from torch.nn.utils.rnn import PackedSequence
-
+from torch.nn.utils.rnn import pad_packed_sequence
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pack_sequence
+from torch.nn.utils.rnn import pad_sequence
 
 __all__ = [
     'pack_sequence',
@@ -20,32 +20,11 @@ __all__ = [
     'pack_padded_sequence'
 ]
 
-
-def pack_sequence(sequences: list) -> PackedSequence:
-    return pack_padded_sequence_(pad_sequence(sequences),
-                                 [v.size(0) for v in sequences])
-
-
 def unpack_sequence(packed_sequence: PackedSequence) -> list:
     return unpad_sequence(*pad_packed_sequence(packed_sequence))
-
-
-def pad_sequence(sequences: list) -> (torch.Tensor, torch.LongTensor):
-    return pad_sequence_(sequences)
 
 
 def unpad_sequence(padded_sequence: torch.Tensor, lengths: list):
     return [padded_sequence[:l, b, ...] for b, l in enumerate(lengths)]
 
-
-def pad_packed_sequence(
-        packed_sequence: PackedSequence
-) -> (torch.Tensor, torch.LongTensor):
-    return pad_packed_sequence_(packed_sequence)
-
-
-def pack_padded_sequence(
-        padded_sequence: torch.Tensor, lengths: torch.LongTensor
-) -> PackedSequence:
-    # ToDo add contiguous if needed
-    return pack_padded_sequence_(padded_sequence, lengths)
+# ToDo add contiguous to pack_padded_sequence if needed
