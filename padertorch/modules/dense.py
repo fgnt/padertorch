377f59e7454a01db20c6dfa87744622a134e88e1
Christoph <cbj@mail.uni-paderborn.de>
2019-01-04 10:47:59 +0100

    update DenseStack implementation

--- padertorch/modules/dense.py
+++ padertorch/modules/dense.py
@@ -1,11 +1,15 @@
-import padertorch as pt
+import collections
 from typing import List
+
 from torch import nn
 import torch.nn.functional as F
-from padertorch.ops.mappings import ACTIVATION_FN_MAP
+
+import padertorch as pt
+from padertorch.ops.mappings import ACTIVATION_FN_MAP, ACTIVATION_NN_MAP
 
 
-class DenseStack(pt.Module):
+# Is a class or a function better? DenseStack vs dense_stack() -> nn.Sequential
+class DenseStack(pt.Module, nn.Sequential):
     def __init__(
             self,
             input_size: int = 513,
@@ -19,26 +23,42 @@ class DenseStack(pt.Module):
         More information to dropout: https://arxiv.org/pdf/1207.0580.pdf
         TODO: Please discuss, if "dense" is the correct name in Torch.
 
+        CB: Why distinguish input_size and num_units? From the signature I
+            would expect num_units[0] == input_size
+        CB: Why activation_fn? activation would be better, especially since it
+            is not a function.
+        CB: Is dropout == 0.5 a proper default? Isn't 0 a better value?
+        CB: num_units shouldn't have a default. Add a doc text explain what is
+            expected.
+
         Args:
             input_size:
             num_units:
             activation_fn:
             dropout: Dropout forget ratio (opposite to TensorFlow)
+
+        >>> DenseStack()
+        DenseStack(
+          (dropout_0): Dropout(p=0.5)
+          (linear_0): Linear(in_features=513, out_features=1024, bias=True)
+          (relu_0): ReLU()
+          (dropout_1): Dropout(p=0.5)
+          (linear_1): Linear(in_features=1024, out_features=1024, bias=True)
+          (relu_1): ReLU()
+          (dropout_2): Dropout(p=0.5)
+          (linear_2): Linear(in_features=1024, out_features=1024, bias=True)
+          (relu_2): ReLU()
+        )
         """
         super().__init__()
-        self.num_units = num_units
-        self.activation_fn = activation_fn
-        self.dropout = dropout
-        self.input_size = input_size
-        l_n_units = [self.input_size] + self.num_units
-        for l_idx, n_units in enumerate(self.num_units):
-            self.__setattr__(f'linear_{l_idx}',
-                     nn.Linear(l_n_units[l_idx], n_units))
+        l_n_units = [input_size] + num_units
 
-    def forward(self, x):
-        for l_idx in range(len(self.num_units)):
-            x = ACTIVATION_FN_MAP[self.activation_fn](
-                self.__getattr__(f'linear_{l_idx}')(
-                    F.dropout(x, self.dropout, self.training))
-            )
-        return x
+        layers = collections.OrderedDict()
+
+        for l_idx, n_units in enumerate(num_units):
+            layers[f'dropout_{l_idx}'] = nn.Dropout(dropout)
+            layers[f'linear_{l_idx}'] = nn.Linear(l_n_units[l_idx], n_units)
+            layers[f'{activation_fn}_{l_idx}'] = \
+                ACTIVATION_NN_MAP[activation_fn]()
+
+        super().__init__(layers)
