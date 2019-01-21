024200070c6cf73752a42c86d49620fb78fa624e
Lukas Drude <mail@lukas-drude.de>
2019-01-21 14:10:53 +0100

    Allow module from filesystem

--- padertorch/base.py
+++ padertorch/base.py
@@ -1,5 +1,8 @@
 import abc
+from pathlib import Path
+import json
 
+import torch
 from torch import nn
 
 from padertorch.configurable import Configurable
@@ -21,6 +24,71 @@ class Module(nn.Module, Configurable, abc.ABC):
         Anything
         """
         pass
+
+    @classmethod
+    def from_config_and_checkpoint(
+            cls,
+            config_path: Path,
+            checkpoint_path: Path,
+            in_config_path: str='trainer.kwargs.model',
+            in_checkpoint_path: str='model',
+    ) -> 'Module':
+        config_path = Path(config_path).expanduser().resolve()
+        checkpoint_path = Path(checkpoint_path).expanduser().resolve()
+
+        assert config_path.is_file(), config_path
+        assert checkpoint_path.is_file(), checkpoint_path
+
+        # Load config
+        assert config_path.is_file(), f'Expected {config_path} is file.'
+        with config_path.open() as fp:
+            configurable_config = json.load(fp)
+        for part in in_config_path.split('.'):
+            configurable_config = configurable_config[part]
+        module = cls.from_config(configurable_config)
+
+        # Load weights
+        checkpoint = torch.load(checkpoint_path)
+        for part in in_checkpoint_path.split('.'):
+            checkpoint = checkpoint[part]
+        module.load_state_dict(checkpoint)
+
+        return module
+
+    @classmethod
+    def from_storage_dir(
+            cls,
+            storage_dir: Path,
+            checkpoint_name: str='ckpt_best_loss.pth',
+            in_config_path: str='trainer.kwargs.model',
+            in_checkpoint_path: str='model',
+    ) -> 'Module':
+        """
+        Assumes fixed folder structure in the default configuration. If this
+        folder structure is not your folder structure, use the function
+        `from_config_and_checkpoint` directly.
+
+        Assumes this structure:
+        storage_dir
+        ├── checkpoints
+        │   └── ckpt_best_loss.pth
+        └── config.json
+
+        Args:
+            storage_dir: Path which was provided during training.
+            checkpoint_name: In case the name is not default.
+            in_config_path: In case you want to load an inner module.
+            in_checkpoint_path: In case you want to load an inner module.
+
+        Returns:
+
+        """
+        return cls.from_config_and_checkpoint(
+            config_path=storage_dir / 'config.json',
+            checkpoint_path=storage_dir / 'checkpoints' / checkpoint_name,
+            in_config_path=in_config_path,
+            in_checkpoint_path=in_checkpoint_path
+        )
 
 
 class Model(Module, Configurable, abc.ABC):
