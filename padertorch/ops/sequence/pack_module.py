6d1eab43d102e2a79db16bd0a4d0bd9041edd6fc
Lukas Drude <mail@lukas-drude.de>
2019-01-23 13:24:51 +0100

    TODOs for pack_module

--- padertorch/ops/sequence/pack_module.py
+++ padertorch/ops/sequence/pack_module.py
@@ -1,9 +1,12 @@
-'''
+"""
 This module allows to switch between three types of tensors:
 packed: PackedSequence
 padded
 list of tensor
-'''
+
+# ToDo add contiguous to pack_padded_sequence if needed
+# TODO: Improve speed of pack_sequence by avoiding padding inbetween
+"""
 import torch
 from torch.nn.utils.rnn import PackedSequence
 from torch.nn.utils.rnn import pad_packed_sequence
@@ -20,11 +23,10 @@ __all__ = [
     'pack_padded_sequence'
 ]
 
+
 def unpack_sequence(packed_sequence: PackedSequence) -> list:
     return unpad_sequence(*pad_packed_sequence(packed_sequence))
 
 
 def unpad_sequence(padded_sequence: torch.Tensor, lengths: list):
     return [padded_sequence[:l, b, ...] for b, l in enumerate(lengths)]
-
-# ToDo add contiguous to pack_padded_sequence if needed
