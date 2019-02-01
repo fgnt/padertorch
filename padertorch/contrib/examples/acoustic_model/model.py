2d384482be30d99bf6b66c93f15b475fe20f5396
Christoph <cbj@mail.uni-paderborn.de>
2019-02-01 14:24:36 +0100

    initial AM example

---
+++ padertorch/contrib/examples/acoustic_model/model.py
@@ -0,0 +1,264 @@
+
+"""
+
+python -m cbj_lib.run <path to sim folder> -- python -m cbj.pytorch.run_am
+
+"""
+
+from dataclasses import dataclass
+
+import numpy as np
+
+import torch
+import einops
+import editdistance
+
+
+import paderbox as pb
+from paderbox.database.iterator import AlignmentReader
+
+from paderbox.database.iterator import FilterException
+
+from paderbox.database.chime import Chime4
+import padertorch as pt
+
+
+def get_blstm_stack(
+        input_size: int,
+        hidden_size: (tuple, list),
+        output_size: int,
+        bidirectional=True,
+        batch_first=False,
+        dropout=0.
+):
+    """
+
+    >>> blstm = get_blstm_stack(2, [3], 3, dropout=0.5)
+    >>> blstm
+    LSTM(2, 3, num_layers=2, dropout=0.5, bidirectional=True)
+
+    """
+    assert batch_first is False, batch_first
+
+    for size in hidden_size:
+        if size != output_size:
+            raise ValueError(
+                f'{input_size}, {hidden_size}, {output_size}: '
+                f'Only shared hidden and output dimension is supported.'
+            )
+
+    num_layers = len(hidden_size) + 1
+
+    if dropout is None or dropout is False:
+        dropout = 0
+
+    return torch.nn.LSTM(
+        input_size,
+        hidden_size=output_size,
+        num_layers=num_layers,
+        bidirectional=bidirectional,
+        batch_first=batch_first,
+        dropout=dropout,
+    )
+
+
+def kaldi_stft(time_signal):
+    # ToDo: window
+    return pb.transform.stft(
+        time_signal=time_signal,
+        size=512,
+        shift=160,
+        axis=-1,
+        window_length=400,
+        pad=False,
+        fading=False,
+
+    )
+
+
+def kaldi_istft(
+        stft_signal,
+):
+    # ToDo: window
+    return pb.transform.istft(
+        stft_signal=stft_signal,
+        size=512,
+        shift=160,
+        window_length=400,
+        fading=False,
+    )
+
+
+def levenshtein_distance(
+    hypothesis, truth
+):
+    assert isinstance(hypothesis, (tuple, list)), (type(hypothesis), hypothesis)
+    assert isinstance(truth, (tuple, list)), (type(truth), truth)
+    return editdistance.eval(hypothesis, truth)
+
+
+class AcousticExperiment(pt.Model):
+    """
+    >>> pb.notebook.pprint(AcousticExperiment.get_config())
+    {'factory': 'model.AcousticExperiment',
+     'db': 'Chime4',
+     'egs_path': '/net/vol/jenkins/kaldi/2018-01-10_15-43-29_a0b71317df1035bd3c6fa49a2b6bb33c801b56ac/egs/chime3/s5',
+     'input_feature': {'type': 'mfcc',
+      'kwargs': {'num_mel_bins': 40,
+       'num_ceps': 40,
+       'low_freq': 20,
+       'high_freq': -400,
+       'delta_order': 2}},
+     'blstm_input_size': 120,
+     'blstm_hidden_size': [150, 150],
+     'blstm_output_size': 150,
+     'blstm_bidirectional': True,
+     'blstm_dropout': 0.3,
+     'dense_input_size': 300,
+     'dense_hidden_size': [500, 500],
+     'dense_output_size': 1983,
+     'dense_activation': 'relu',
+     'dense_dropout': 0.3}
+
+    """
+
+    def __init__(
+            self,
+            db='Chime4',
+            egs_path='/net/vol/jenkins/kaldi/2018-01-10_15-43-29_a0b71317df1035bd3c6fa49a2b6bb33c801b56ac/egs/chime3/s5',
+            input_feature={
+                'type': 'mfcc',
+                'kwargs': {
+                    # check that these parameters are the mfcc_high_res
+                    'num_mel_bins': 40,
+                    'num_ceps': 40,
+                    'low_freq': 20,
+                    'high_freq': -400,
+                    'delta_order': 2,
+                }
+            },
+            blstm_input_size=40 * 3,
+            blstm_hidden_size=(150, 150),
+            blstm_output_size=150,
+            blstm_bidirectional=True,
+            blstm_dropout=0.3,
+            dense_input_size=150 * 2,  # 2 times blstm_output_size
+            dense_hidden_size=(500, 500),
+            dense_output_size=1983,  # Chime4 specific
+            dense_activation='relu',
+            dense_dropout=0.3,
+    ):
+        super().__init__()
+        if db == 'Chime4':
+            self.db = Chime4(
+                egs_path=egs_path
+            )
+        else:
+            raise NotImplementedError(db)
+        self.input_feature = input_feature
+        self.blstm = get_blstm_stack(
+            input_size=blstm_input_size,
+            hidden_size=blstm_hidden_size,
+            output_size=blstm_output_size,
+            bidirectional=blstm_bidirectional,
+            batch_first=False,
+            dropout=blstm_dropout
+        )
+        self.dense = pt.modules.fully_connected_stack(
+            input_size=dense_input_size,
+            hidden_size=dense_hidden_size,
+            output_size=dense_output_size,
+            activation=dense_activation,
+            dropout=dense_dropout,
+        )
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+    def get_iterable(self, dataset):
+        if isinstance(self.db, Chime4):
+            it = self.db.get_iterator_by_names(dataset)
+            it = it.map(AlignmentReader(
+                alignments=self.db.state_alignment,
+                example_id_map_fn=self.db.example_id_map_fn
+            ))
+            return it
+        else:
+            raise TypeError(self.db)
+
+    def transform(self, example):
+        if self.input_feature['type'] == 'stft':
+            Observation = np.abs(kaldi_stft([
+                pb.io.load_audio(file)
+                for file in example['audio_path']['observation']
+            ]))
+        elif self.input_feature['type'] in ['mfcc']:
+            Observation = np.array(pb.kaldi.mfcc.compute_mfcc_feats(
+                example['audio_path']['observation'],
+                is_scp=True,
+                stacked=True,
+                **self.input_feature['kwargs'],
+            ))
+        else:
+            raise ValueError(self.input_feature)
+
+        Obs = einops.rearrange(
+            Observation,
+            'channel time freq -> time channel freq',
+        ).astype(np.float32)
+
+        example_id = example['example_id']
+
+        if 'alignment' not in example:
+            raise FilterException(f'{example_id} has not alignment')
+
+        alignment = torch.tensor([example['alignment'].astype(np.int64)] * 6).t()
+
+        return self.NNInput(
+            Observation=Obs,
+            alignment=alignment,
+            kaldi_transcription=example['kaldi_transcription'].split(),
+        )
+
+    @dataclass
+    class NNInput:
+        Observation: torch.tensor
+        alignment: torch.tensor
+        kaldi_transcription: tuple
+
+    def forward(self, example: NNInput):
+        Observation = example.Observation
+
+        hidden, _ = self.blstm(Observation)
+        predict = self.dense(hidden)
+
+        return self.NNOutput(
+            predict=predict
+        )
+
+    @dataclass
+    class NNOutput:
+        predict: torch.tensor
+
+    def review(self, example: NNInput, outputs: NNOutput):
+
+        predict = outputs.predict.data.cpu().numpy()
+
+        assert np.all(np.isfinite(predict)), predict
+
+        ce = self.criterion(
+            torch.einsum('tbf->tfb', outputs.predict),
+            example.alignment,
+        )
+
+        with torch.no_grad():
+            predicted = torch.argmax(outputs.predict.data, -1)
+            tmp = (predicted == example.alignment)
+            accuracy = tmp.sum().item() / tmp.numel()
+
+        return {
+            'losses': {
+                'ce': ce,
+            },
+            'scalars': {
+                'acc': accuracy,
+            }
+        }
---
+++ padertorch/contrib/examples/acoustic_model/train.py
@@ -0,0 +1,135 @@
+"""
+
+# Start command:
+# - Change STORAGE_ROOT
+export STORAGE_ROOT=/net/vol/$USER/sacred/torch/examples
+mkdir -p $STORAGE_ROOT/acoustic_model
+python -m padertorch.contrib.examples.acoustic_model.train
+
+# One liner:
+export STORAGE_ROOT=/net/vol/$USER/sacred/torch/examples && mkdir -p $STORAGE_ROOT/acoustic_model &&
+python -m padertorch.contrib.examples.acoustic_model.train
+
+"""
+import os
+from pathlib import Path
+
+import arrow
+import sacred
+import sacred.commands
+
+import paderbox as pb
+import padertorch as pt
+
+from padertorch.contrib.ldrude.utils import (
+    decorator_append_file_storage_observer_with_lazy_basedir
+)
+from padertorch.contrib.cb import (
+    get_new_folder,
+    write_makefile_and_config,
+)
+
+from padertorch.contrib.examples.acoustic_model.model import AcousticExperiment
+
+ex = sacred.Experiment('AM')
+
+
+def get_basedir():
+    basedir = (Path(
+        os.environ['STORAGE_ROOT']
+    ) / 'acoustic_model').expanduser().resolve()
+    if not basedir.is_dir():
+        raise FileNotFoundError(
+            f'No such directory: {basedir}\n'
+            f'run `mkdir -p {basedir}`'
+        )
+    return basedir
+
+
+@ex.config
+def config():
+    dataset_train = 'tr05_simu'
+    dataset_dev = 'dt05_simu'
+
+    trainer = pb.utils.nested.deflatten({
+        'model.factory': AcousticExperiment,
+        'model.db': 'Chime4',
+        'optimizer.factory': pt.optimizer.Adam,
+        'summary_trigger': (1000, 'iteration'),
+        'checkpoint_trigger': (1, 'epoch'),
+        'max_trigger': (50, 'epoch'),
+        'loss_weights': None,
+        'keep_all_checkpoints': True,
+        'storage_dir': None,
+    })
+    pt.Trainer.get_config(trainer)
+
+    if trainer['storage_dir'] is None:
+        trainer['storage_dir'] = get_new_folder(get_basedir(), mkdir=False)
+
+
+@decorator_append_file_storage_observer_with_lazy_basedir(ex)
+def basedir(_config):
+    return Path(_config['trainer']['storage_dir']) / 'sacred'
+
+
+@ex.capture
+def prepare_and_train(
+        _config,
+        dataset_train,
+        dataset_dev,
+        _run,
+        resume=False
+):
+    print('Start time:', arrow.now().format('YYYY-MM-DD HH:mm:ss'))
+    storage_dir = Path(_config['trainer']['storage_dir'])
+    try:
+        sacred.commands.print_config(_run)
+        storage_dir = Path(storage_dir)
+        print('Storage dir:', storage_dir)
+
+        trainer = pt.Trainer.from_config(_config['trainer'])
+
+        model: AcousticExperiment = trainer.model
+        print('model:')
+        print(model)
+
+        it_tr = model.get_iterable(dataset_train).shuffle(reshuffle=False)
+        it_tr = it_tr.map(model.transform)
+        it_dt = model.get_iterable(dataset_dev)
+        it_dt = it_dt.map(model.transform)
+
+        print('it_tr:')
+        print(it_tr)
+        print('it_dt:')
+        print(it_dt)
+
+        print('Storage dir:', storage_dir)
+
+        trainer.test_run(it_tr.catch(), it_dt.catch())
+        trainer.train(
+            it_tr.prefetch(4, 8, catch_filter_exception=True),
+            it_dt.prefetch(4, 8, catch_filter_exception=True),
+            resume=resume,
+        )
+    finally:
+        print('Storage dir:', storage_dir)
+        print('End time:', arrow.now().format('YYYY-MM-DD HH:mm:ss'))
+
+
+@ex.command
+def resume():
+    return prepare_and_train(resume=True)
+
+
+@ex.main
+def main(_config, _run):
+    write_makefile_and_config(
+        _config['trainer']['storage_dir'], _config, _run,
+        backend='yaml'
+    )
+    return prepare_and_train(resume=False)
+
+
+if __name__ == '__main__':
+    ex.run_commandline()
