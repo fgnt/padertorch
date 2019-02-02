e9175f12166679f4b6c23ab4af7c3ecdf6a06586
Christoph <cbj@mail.uni-paderborn.de>
2019-02-02 18:42:08 +0100

    from_config_and_checkpoint: better exception msg

--- padertorch/base.py
+++ padertorch/base.py
@@ -50,7 +50,10 @@ class Module(nn.Module, Configurable, abc.ABC):
         # Load weights
         checkpoint = torch.load(checkpoint_path)
         for part in in_checkpoint_path.split('.'):
-            checkpoint = checkpoint[part]
+            try:
+                checkpoint = checkpoint[part]
+            except KeyError:
+                raise ValueError(part, in_checkpoint_path, checkpoint)
         module.load_state_dict(checkpoint)
 
         return module
