49110da43648ac2c2fda57eb5a1ed84e76b17cf8
Thomas Glarner <tglarner@mail.upb.de>
2019-02-03 21:29:20 +0100

    configurable.py: Fix docstrings + PEP8, rename some variables, whitespace.

--- padertorch/configurable.py
+++ padertorch/configurable.py
@@ -5,34 +5,36 @@ itself, e.g. a structured variational autoencoder or a farfield speech
 enhancer.
 
 You can instantiate a child of `Configurable` either with an `__init__`
-resulting in an unparameterized module, or if you instantiate it with
+resulting in an unparameterized module. If you instantiate it with
 `from_config` you get a configured module.
 
 If modules contain modules, look out for examples on how to override
-`finalize_dogmatic_config` (padertorch.contrib.examples.configurable).
+`finalize_docmatic_config` (padertorch.contrib.examples.configurable).
 In most cases, when you want to provide an instance as
 a parameter to the `__init__` you can instead provide the parameters which were
-used for that instance in your modified `finalize_dogmatic_config`.
+used for that instance in your modified `finalize_docmatic_config`.
+
 """
-import sys
-import inspect
-import importlib
 import collections
+import importlib
+import inspect
+import sys
 from pathlib import Path
 
 import paderbox as pb
 
 
 class Configurable:
-    """
-    Motivation: ToDo: reproducability, adjustable
+    """Allow subclasses to be configured automatically from JSON config files.
+
+    Motivation: TODO: reproducability, adjustable
 
     Each factory is a configurable object (e.g. class constructor and
     function). The configurable object can be described with the factory object
     or the python path to the factory object and the kwargs of that function.
     To instantiate the object, the factory is called with the kwargs.
-    Note: Inside the config be again a configurable object, but later more about
-    this. First an example:
+    Note: Inside the config might be again a configurable object,
+    but later more about this.
 
     Example::
 
@@ -154,7 +156,10 @@ class Configurable:
     'in_features' disappears because the config object that is given to
     `finalize_dogmatic_config` is a dogmatic dict. That means the updates have
     a higher priority than the assigned value. This behaviour is similar to
-    Sacred (https://sacred.readthedocs.io/en/latest/configuration.html#updating-config-entries).
+    Sacred[1].
+
+    [1]
+    https://sacred.readthedocs.io/en/latest/configuration.html#updating-config-entries.
 
     Example::
 
@@ -183,7 +188,7 @@ class Configurable:
     Another use case for this behaviour are depended config entries (e.g.
     NN input size depends on selected input features).
 
-    # ToDo: This text ist outdated and needs to be reformulated
+    # TODO: This text ist outdated and needs to be reformulated
     # The values in the config are enforced to have the updated value.
     # (example below) i.e. when the update set 'activation' to be 'sigmoid'
     # the statement `config['activation'] = 'relu'` has no effect.
@@ -260,7 +265,7 @@ class Configurable:
         )
     """
 
-    # ToDo: Drop get_signature?
+    # TODO: Drop get_signature?
     @classmethod
     def get_signature(cls):
         raise NotImplementedError('get_signature should no longer be used'
@@ -269,12 +274,13 @@ class Configurable:
 
     @classmethod
     def finalize_dogmatic_config(cls, config):
-        """
-        🔥🔥 If you overwrite this function you need to know, what a      🔥🔥
+        """Finalize the config. Should be overridden in subclass.
+
+        🔥🔥 If you override this function you need to know what a        🔥🔥
         🔥🔥 dogmatic dict is. Within this method, Python behaves as if   🔥🔥
         🔥🔥 you are in a Sacred config.                                  🔥🔥
 
-        ToDo: doctext (for now see above Configurable doctext with the
+        TODO: doctext (for now see above Configurable doctext with the
               examples)
         """
         pass
@@ -283,16 +289,25 @@ class Configurable:
     def get_config(
             cls,
             updates=None,
-            # out_config=None,
     ):
+        """Return the config as a JSON serializeable dict.
 
+        Args:
+            updates: Optional dict containing updates for the config.
+
+        Returns:
+            The serializeable config.
+            If updates is not given, returns the default config.
+            If updates is given, return the updated config.
+
+        """
         if cls.__module__ == '__main__':
             # When a class is defined in the main script, it will be
             # __main__.<ModelName>, but it should be <script>.<ModelName>.
-            # This fix it, when the script is called with
+            # This fix is active when the script is called with
             # "python -m <script> ..."
             # but not when it is called with "python <script>.py ..."
-            cls = import_class(class_to_str(cls))
+            cls_ = import_class(class_to_str(cls))
 
         if updates is None:
             updates = {}
@@ -301,14 +316,14 @@ class Configurable:
             config = _sacred_dogmatic_to_dict(updates)
 
         if 'factory' not in config:
-            config['factory'] = cls
+            config['factory'] = cls_
         else:
             config['factory'] = import_class(config['factory'])
             if inspect.isclass(config['factory']) \
                     and issubclass(config['factory'], Configurable):
                 # When subclass of Configurable expect proper subclass
-                assert issubclass(import_class(config['factory']), cls), (
-                    config['factory'], cls)
+                assert issubclass(import_class(config['factory']), cls_), (
+                    config['factory'], cls_)
 
         config = _DogmaticConfig.normalize(config)
 
@@ -329,7 +344,8 @@ class Configurable:
             cls,
             config,
     ) -> 'Configurable':
-        # ToDo: assert do not use defaults
+        """Produce a Configurable instance from a valid config."""
+        # TODO: assert do not use defaults
         assert 'factory' in config, (cls, config)
         if cls is not Configurable:
             assert issubclass(import_class(config['factory']), cls), \
@@ -350,7 +366,7 @@ class Configurable:
 
 
 def test_config(config, updates):
-    # Test if the kwargs are valid
+    """Test if the config updates are valid."""
     sig = inspect.signature(import_class(config['factory']))
 
     # Remove default -> force completely described
@@ -396,8 +412,8 @@ def test_config(config, updates):
             ) from ex
         else:
             raise TypeError(
-                f'The test, if the call {factory}(**kwargs) would '
-                f'be successful, failed.\n'
+                f'The test whether the call {factory}(**kwargs) would '
+                f'be successful failed.\n'
                 f'Where\n'
                 f'     kwargs: {kwargs}\n'
                 f'     signature: {sig}\n'
@@ -422,23 +438,25 @@ def test_config(config, updates):
 
 
 def fix_doctext_import_class(locals_dict):
-    """
-    Use this function inside a doctest as
-    `>>> fix_doctext_import_class(locals())`
+    """Allow classes defined in a doctest to be imported.
 
-    This is necessary, because classes defined in a doctest could not be
+    This is necessary because classes defined in a doctest can not not be
     imported.
 
-    >>> abc = 1
-    >>> class Foo: pass
-    >>> import_class(class_to_str(Foo))  # doctest: +ELLIPSIS
-    Traceback (most recent call last):
-    ...
-    AttributeError: Module ... 'configurable' ... has no attribute Foo. ...
+    Use this function inside a doctest as
+        >>> fix_doctext_import_class(locals())
 
-    >>> fix_doctext_import_class(locals())
-    >>> import_class(class_to_str(Foo))
-    <class 'configurable.Foo'>
+    Example::
+        >>> abc = 1
+        >>> class Foo: pass
+        >>> import_class(class_to_str(Foo))  # doctest: +ELLIPSIS
+        Traceback (most recent call last):
+        ...
+        AttributeError: Module ... 'configurable' ... has no attribute Foo. ...
+
+        >>> fix_doctext_import_class(locals())
+        >>> import_class(class_to_str(Foo))
+        <class 'configurable.Foo'>
     """
     cache = {}
 
@@ -469,17 +487,16 @@ def fix_doctext_import_class(locals_dict):
 
 
 def import_class(name: str):
-    """
-    Imports the str and returns the imported object.
+    """Import the str and return the imported object.
 
     Opposite of class_to_str.
-    
+
     >>> import padertorch
     >>> import_class(padertorch.Model)
     <class 'padertorch.base.Model'>
     >>> import_class('padertorch.Model')
     <class 'padertorch.base.Model'>
-    
+
     """
     if not isinstance(name, str):
         return name
@@ -515,8 +532,7 @@ def resolve_main_python_path() -> str:
 
 
 def class_to_str(cls):
-    """
-    Convert a class to an importable str.
+    """Convert a class to an importable str.
 
     Opposite of import_class.
 
@@ -526,10 +542,9 @@ def class_to_str(cls):
     >>> class_to_str('padertorch.Model')
     'padertorch.base.Model'
 
-    ToDo: fix __main__ for scripts in packages that are called with shell
+    TODO: fix __main__ for scripts in packages that are called with shell
           path (path/to/script.py) and not python path (path.to.script).
     """
-
     if isinstance(cls, str):
         cls = import_class(cls)
 
@@ -550,7 +565,7 @@ def _split_factory_kwargs(config):
     kwargs = config.copy()
     factory = kwargs.pop('factory')
     return factory, kwargs
-    
+
 
 def config_to_instance(config):
     if isinstance(config, dict):
@@ -578,7 +593,7 @@ def config_to_instance(config):
 
 
 class NestedChainMap(collections.ChainMap):
-    """
+    """A ChainMap that works on either nested dicts or DogmaticDicts.
 
     This class is similar to collections.ChainMap.
     Differences:
@@ -590,7 +605,7 @@ class NestedChainMap(collections.ChainMap):
             When a earlier mapping defines the value for that key, __getitem__
             will return that value
 
-
+    Examples::
     >>> from IPython.lib.pretty import pprint
     >>> a = {'1': {'1_1': 1}, '2': {'2_2': 2}, '3': 3}
     >>> b = {'1': {'1_2': 3}, '2': {'2_1': 3, '2_2': 4}}
@@ -614,11 +629,13 @@ class NestedChainMap(collections.ChainMap):
     >>> pprint(c.to_dict())
     {'1': {'1_2': 200, '1_1': 1}, '2': {'2_1': 3, '2_2': 2}, '3': 3}
     """
+
     def __init__(
             self,
             *maps,
             mutable_idx=0,
     ):
+        """Construct NestedChainMap from an arbitrary number of mappings."""
         self.subs = {}
         super().__init__(*maps)
         self.mutable_idx = mutable_idx
@@ -675,8 +692,8 @@ class NestedChainMap(collections.ChainMap):
             #     raise Exception(
             #         f'Tried to get the value for the key "{item}" in this '
             #         f'NestedChainMap.\n'
-            #         f'Expect that all values in the maps are dicts or none is'
-            #         f'a dict:\n'
+            #         f'Expect that all values in the maps are dicts '
+            #         f'or none is a dict:\n'
             #         f'{pretty(self)}'
             #     )
             m: dict
@@ -723,52 +740,41 @@ class NestedChainMap(collections.ChainMap):
 
 
 def _sacred_dogmatic_to_dict(config):
-    """
+    """Perform a mutable-safe conversion of nested sacred DogmaticDict.
+
     Takes a nested structure as input and sets all values that are defined
     from sacred as "fixed". Returns the nested structure with all updates
     applied from sacred.
     """
     import sacred.config.custom_containers
-    import paderbox as pb
 
     if isinstance(config, sacred.config.custom_containers.DogmaticDict):
+        merge_keys = [
+            *config.keys(),
+            *_sacred_dogmatic_to_dict(config.fallback).keys(),
+        ]
         return pb.utils.nested.nested_merge(
-            {
-                k: _sacred_dogmatic_to_dict(config[k])
-                for keys in [
-                config.keys(),
-                _sacred_dogmatic_to_dict(config.fallback).keys(),
-            ]
-                for k in keys
-            }, {
-                k: _sacred_dogmatic_to_dict(v)
-                for k, v in config.fixed.items()
-            }
+            {k: _sacred_dogmatic_to_dict(config[k]) for k in merge_keys},
+            {k: _sacred_dogmatic_to_dict(v) for k, v in config.fixed.items()}
         )
     elif isinstance(config, dict):
-        return {
-            k: _sacred_dogmatic_to_dict(v)
-            for k, v in config.items()
-        }
+        return {k: _sacred_dogmatic_to_dict(v) for k, v in config.items()}
     else:
         return config
 
 
 class _DogmaticConfig:
-    """
-    This class is an implementation detail of Configurable
-    """
+    """This class is an implementation detail of Configurable."""
 
     @staticmethod
     def get_signature(factory):
-        """
-        Checks signature of the factory. If parameters have defaults, return
-        these in a dictionary.
+        """Check signature of the factory.
+
+        If parameters have defaults, return these in a dictionary.
 
         Returns:
 
         """
-
         sig = inspect.signature(factory)
         defaults = {}
         param: inspect.Parameter
@@ -778,10 +784,11 @@ class _DogmaticConfig:
         return defaults
 
     @classmethod
-    def _force_factory_type(self, factory):
-        """
-        This is a placeholder until it is finally decided, if the factory
-        should be a str or a class in finalize_dogmatic_config.
+    def _force_factory_type(cls, factory):
+        """Convert factory from str to cls object if necessary.
+
+        This is a placeholder until it is finally decided if the factory
+        should be a str or a class in finalize_docmatic_config.
 
         Options:
          - class
@@ -794,18 +801,18 @@ class _DogmaticConfig:
 
     @classmethod
     def normalize(cls, dictionary):
-        """
-        Normalize the nested dictionary.
-         - The value of factory keys are forced to be the object and not the
-           str. (i.e. import_class).
-         - pathlib.Path to str
+        """Normalize the nested dictionary.
+
+        - The value of factory keys are forced to be the object and
+          not the str. (i.e. import_class).
+        - pathlib.Path to str
         """
         if isinstance(dictionary, collections.Mapping):
             if 'factory' in dictionary:
                 dictionary['factory'] = cls._force_factory_type(
                     dictionary['factory']
                 )
-            for k, v in list(dictionary.items()):
+            for v in dictionary.values():
                 cls.normalize(v)
         elif isinstance(dictionary, (tuple, list)):
             dictionary = [
@@ -900,8 +907,7 @@ class _DogmaticConfig:
         self.data[key] = self.normalize(value)
 
     def __getitem__(self, key):
-        """
-        Returns the value for the key.
+        """Return the value for the key.
 
         When the value is not a dict, directly return the value.
         When the value is a dict and does not contain "factory" as key,
@@ -910,7 +916,7 @@ class _DogmaticConfig:
         When the dict contains the key "factory", freeze the mutable_idx of
         this _DogmaticConfig instance and update the kwargs
         (i.e. get the defaults from the signature and call
-        finalize_dogmatic_config is it exists.)
+        finalize_dogmatic_config if it exists.)
 
         """
         if 'factory' in self.data \
@@ -940,7 +946,7 @@ class _DogmaticConfig:
                 # (delta, self.data.keys(), parameter_names)
                 from IPython.lib.pretty import pretty
                 raise Exception(
-                    f'Got for the factory {factory} to much keywords.\n'
+                    f'Too many keywords for the factory {factory}.\n'
                     f'Delta: {delta}\n'
                     f'signature: {inspect.signature(factory)}\n'
                     f'current config with fallbacks:\n{pretty(self.data)}'
@@ -970,20 +976,22 @@ class _DogmaticConfig:
         return value
 
     def to_dict(self):
-        d = {}
+        """Export the Configurable object to a dict."""
+        result_dict = {}
         for k in self.keys():
             try:
                 v = self[k]
-            except KeyError as e:
+            except KeyError as ex:
                 from IPython.lib.pretty import pretty
                 if 'factory' in self.keys() and k != 'factory':
                     # KeyError has a bad __repr__, use Exception
+                    missing_keys = set(self.keys()) - set(self.data.keys())
                     raise Exception(
                         f'KeyError: {k}\n'
                         f'signature: {inspect.signature(self["factory"])}\n'
-                        f'missing keys: {set(self.keys()) - set(self.data.keys())}\n'
+                        f'missing keys: {missing_keys}\n'
                         f'self:\n{pretty(self)}'
-                    ) from e
+                    ) from ex
                 else:
                     # KeyError has a bad __repr__, use Exception
                     # Can this happen?
@@ -991,19 +999,20 @@ class _DogmaticConfig:
                         f'{k}\n'
                         f'signature: {inspect.signature(self["factory"])}'
                         f'self:\n{pretty(self)}'
-                    ) from e
+                    ) from ex
 
-            if 'factory' == k:
+            if k == 'factory':
                 v = class_to_str(v)
             if isinstance(v, self.__class__):
                 v = v.to_dict()
-            d[k] = v
+            result_dict[k] = v
 
-        return d
+        return result_dict
 
     def __repr__(self):
         maps = ', '.join([repr(m) for m in self.data.maps])
-        return f'{self.__class__.__name__}({maps}, mutable_idx={self.data.mutable_idx})'
+        return (f'{self.__class__.__name__}({maps},'
+                f' mutable_idx={self.data.mutable_idx})')
 
     def _repr_pretty_(self, p, cycle):
         if cycle:
