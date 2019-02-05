b720614deb4cd66bb75352bd3b208e48c09b5f0d
Janek Ebbers <ebbers@nt.upb.de>
2019-02-05 10:00:45 +0100

    extend conv1d test

--- tests/contrib/je/test_conv1d.py
+++ tests/contrib/je/test_conv1d.py
@@ -1,5 +1,4 @@
 import unittest
-
 import torch
 from padertorch.contrib.je.conv1d import TCN
 
@@ -16,26 +15,28 @@ class TestTCN(unittest.TestCase):
         x = torch.ones(batch_size, input_size, n_frames)
         h = torch.ones(batch_size, condition_size, n_frames)
         for n_scales in [None, 1, 2]:
+            for pool_size in [None, 2, 4]:
 
-            enc = TCN(
-                **TCN.get_config(
-                    updates=dict(
-                        input_size=input_size, hidden_sizes=256,
-                        output_size=latent_dim, condition_size=condition_size,
-                        n_scales=n_scales, norm='batch'
+                enc = TCN.from_config(
+                    TCN.get_config(
+                        updates=dict(
+                            input_size=input_size, hidden_sizes=256,
+                            output_size=latent_dim, condition_size=condition_size,
+                            n_scales=n_scales, norm='batch', pool_size=pool_size
+                        )
                     )
-                )['kwargs']
-            )
-            z, pool_indices = enc(x, h)
-            self.assertEquals(z.shape, (batch_size, latent_dim, n_frames))
-            dec = TCN(
-                **TCN.get_config(
-                    updates=dict(
-                        input_size=latent_dim, hidden_sizes=256,
-                        output_size=input_size, condition_size=condition_size,
-                        transpose=True, n_scales=n_scales, norm='batch'
+                )
+                z, pool_indices = enc(x, h)
+                self.assertEquals(z.shape, (batch_size, latent_dim, n_frames))
+                dec = TCN.from_config(
+                    TCN.get_config(
+                        updates=dict(
+                            input_size=latent_dim, hidden_sizes=256,
+                            output_size=input_size, condition_size=condition_size,
+                            transpose=True, n_scales=n_scales, norm='batch',
+                            pool_size=pool_size
+                        )
                     )
-                )['kwargs']
-            )
-            x_hat = dec(z, h, pool_indices=pool_indices[::-1])
-            self.assertEquals(x_hat.shape, (batch_size, input_size, n_frames))
+                )
+                x_hat = dec(z, h, pool_indices=pool_indices[::-1])
+                self.assertEquals(x_hat.shape, (batch_size, input_size, n_frames))
