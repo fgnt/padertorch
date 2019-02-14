1996e3cea6b00fa977eb07745c131573548de4c4
Thomas Glarner <tglarner@mail.upb.de>
2019-02-15 00:20:48 +0100

    Add tensor ops for matrix batches.

--- padertorch/ops/tensor.py
+++ padertorch/ops/tensor.py
@@ -32,3 +32,31 @@ def move_axis(a: torch.Tensor, source: int, destination: int):
 
 def zeros(shape, dtype=None):
     return torch.zeros(*shape, dtype=dtype)
+
+
+def matrix_diag(x):
+    """Apply the diag matrix operation along the batch axis."""
+    if x.dim() == 1:
+        return torch.diag(x)
+    feature_dim = x.shape[-1]
+    mat = x.reshape((-1, feature_dim))
+    diags = torch.stack([torch.diag(vec) for vec in mat])
+    return diags.reshape((*x.shape, feature_dim))
+
+
+def matrix_eye_like(x):
+    """Apply the eye matrix operation along the batch axis."""
+    if x.dim() == 1:
+        return torch.eye(*tensor.size())
+    feature_dim = x.shape[-1]
+    mat = x.reshape(-1, feature_dim)
+    eyes = torch.stack([torch.eye(*vec.size()) for vec in mat])
+    return eyes.reshape((*x.shape, feature_dim))
+
+
+def batch_tril(x):
+    """Apply torch.tril along the minibatch axis."""
+    matrix_dims = x.shape[-2:]
+    mats = x.reshape((-1, *matrix_dims))
+    trils = torch.stack([torch.tril(mat) for mat in mats])
+    return trils.reshape(x.shape)
