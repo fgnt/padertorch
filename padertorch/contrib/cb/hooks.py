801232205b4d3b110a282fe75cb6b9cad52111f7
Christoph <cbj@mail.uni-paderborn.de>
2019-02-25 09:29:02 +0100

    contrib: Update CPUTimeLimitExceededHookTrigger

--- padertorch/contrib/cb/hooks.py
+++ padertorch/contrib/cb/hooks.py
@@ -1,23 +1,51 @@
 import signal
-from padertorch.train.hooks import StopTrainingHook
-from padertorch.train.trigger import Trigger
+from padertorch.train.hooks import StopTrainingHook, StopTraining
+from padertorch.train.trigger import Trigger, IntervalTrigger
 
 
 class CPUTimeLimitExceededHookTrigger(Trigger):
+    """
+    Graceful shutdown of Training.
+
+    Shutdown after next iteration (i.e. as fast as possible, finish validation)
+    $ ccssignal XCPU <reqid>
+    Use `ccsalloc ... --notifyjob=XCPU,60m ...` to let ccs send the signal.
+
+    Shutdown after next epoch (i.e. finish current epoch, good iterator state)
+    $ ccssignal USR1 <reqid>  # Shutdown after next epoch
+
+    """
     def __init__(self):
         self._SIGXCPU_received = False
-        signal.signal(signal.SIGXCPU, self.handler)
+        self._SIGUSR1_received = False
+        signal.signal(signal.SIGXCPU, self.handler_SIGXCPU)
+        signal.signal(signal.SIGUSR1, self.handler_SIGUSR1)
 
-    def handler(self, signum, frame):
+        self.epoch_trigger = IntervalTrigger(1, 'epoch')
+
+    def handler_SIGXCPU(self, signum, frame):
         print('Received SIGXCPU: CPU time limit exceeded')
         print('Graceful shutdown training')
         self._SIGXCPU_received = True
+
+    def handler_SIGUSR1(self, signum, frame):
+        print('Received SIGUSR1: User-defined signal 1.')
+        print(f'Graceful shutdown training when epoch '
+              f'{self.epoch_trigger.last + 1} is finished')
+        self._SIGUSR1_received = True
 
     def set_last(self, iteration, epoch):
         pass
 
     def __call__(self, iteration, epoch):
-        return self._SIGXCPU_received
+        return (
+            (
+                self.epoch_trigger(iteration, epoch)
+                and self._SIGUSR1_received
+            )
+            or self._SIGXCPU_received
+        )
+
 
 
 class CPUTimeLimitExceededHook(StopTrainingHook):
@@ -48,10 +76,39 @@ if __name__ == '__main__':
         epoch = 0
 
 
-    while True:
-        print(time.perf_counter())
-        if hook.pre_step(Trainer()):
-            break
-        time.sleep(1)
+    try:
+        while True:
+            print(time.perf_counter())
+            if hook.pre_step(Trainer()):
+                break
+            time.sleep(1)
+    except StopTraining:
+        print('StopTraining')
+    thread.join()
 
+
+    hook = CPUTimeLimitExceededHook()
+
+    def killer():
+        time.sleep(0.5)
+        os.kill(pid, signal.SIGUSR1)
+
+    thread = Thread(target=killer)
+    thread.start()
+
+    class Trainer:
+        iteration = 0
+        epoch = 0
+
+    try:
+        while True:
+            Trainer.iteration += 1
+            if (Trainer.iteration % 5) == 0:
+                Trainer.epoch += 1
+            print(time.perf_counter(), Trainer.iteration, Trainer.epoch)
+            if hook.pre_step(Trainer()):
+                break
+            time.sleep(1)
+    except StopTraining:
+        print('StopTraining')
     thread.join()
