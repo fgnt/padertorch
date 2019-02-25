6a38fe53474c72390f529c3500d0839dd619a94c
Christoph <cbj@mail.uni-paderborn.de>
2019-02-25 09:29:34 +0100

    contrib: fix ReviewSummary

--- padertorch/contrib/cb/summary.py
+++ padertorch/contrib/cb/summary.py
@@ -1,4 +1,7 @@
 
+import collections
+
+import padertorch as pt
 
 # loss: torch.Tenso r =None,
 # losses: dict =None,
@@ -8,7 +11,15 @@
 # images: dict =None,
 
 
-class ReviewSummary:
+class ReviewSummary(collections.Mapping):
+    """
+    >>> ReviewSummary()
+    """
+
+    _keys = set(pt.train.hooks.SummaryHook.empty_summary_dict().keys()) | {
+        'loss', 'losses'
+    }
+
     def __init__(self, prefix='', _data=None):
         if _data is None:
             _data = {}
@@ -23,6 +34,7 @@ class ReviewSummary:
 
     def add_scalar(self, name, value):
         # Save the mean of all added values
+        value = pt.utils.to_numpy(value, detach=True)
         self.data.setdefault(
             'scalars',
             {}
@@ -31,28 +43,23 @@ class ReviewSummary:
             []
         ).append(value)
 
-    def add_image(self, name, value):
+    def add_image(self, name, image):
         # Save the last added value
+        image = pt.utils.to_numpy(image, detach=True)
         self.data.setdefault(
             'images',
             {}
-        )[f'{self.prefix}{name}'] = value
+        )[f'{self.prefix}{name}'] = image
 
     def __contains__(self, item):
         return item in self.data
 
-    def __getitem__(self, item):
-        if item == 'scalars':
-            return {
-                k: sum(v) / len(v)
-                for k, v in self.data[item].items()
-            }
-        elif item == 'images':
-            return self.data[item]
-        else:
-            raise KeyError()
+    def __getitem__(self, key):
+        assert key in self._keys, (key, self._keys)
+        return self.data[key]
 
     def __setitem__(self, key, value):
+        assert key in self._keys, (key, self._keys)
         self.data[key] = value
 
     def get(self, item, default):
@@ -63,3 +70,9 @@ class ReviewSummary:
 
     def setdefault(self, key, default):
         self.data.setdefault(key, default)
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
