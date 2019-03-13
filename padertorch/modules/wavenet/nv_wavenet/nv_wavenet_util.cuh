216ef47f338c230c3d328d46d52df5d9d938d491
Janek Ebbers <ebbers@nt.upb.de>
2019-03-13 13:23:21 +0100

    Added hint about required Compute Capability in nv_wavenet

--- padertorch/modules/wavenet/nv_wavenet/nv_wavenet_util.cuh
+++ padertorch/modules/wavenet/nv_wavenet/nv_wavenet_util.cuh
@@ -34,7 +34,7 @@
 #define gpuErrChk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true) {
     if (code != cudaSuccess) {
-        fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+        fprintf(stderr, "GPUassert: %s %s %d. Do note that nv-wavenet requires Compute Capability 6.0 or later (https://developer.nvidia.com/cuda-gpus).\n", cudaGetErrorString(code), file, line);
         if (abort) exit(code);
     }
 }
