83b44e525650aeea0ac73c7980d02af1c5d302f8
Janek Ebbers <ebbers@nt.upb.de>
2019-03-19 11:11:49 +0100

    General Pool1d and Unpool1d allowing max and avg (un)pooling

--- tests/contrib/je/test_conv1d.py
+++ tests/contrib/je/test_conv1d.py
@@ -15,31 +15,35 @@ class TestTCN(unittest.TestCase):
         x = torch.ones(batch_size, input_size, n_frames)
         h = torch.ones(batch_size, condition_size, n_frames)
         for n_scales in [None, 1, 2]:
-            for pool_size in [1, 2]:
-                for padding in ['both', None]:
-                    enc = TCN.from_config(
-                        TCN.get_config(
-                            updates=dict(
-                                input_size=input_size, hidden_sizes=256,
-                                output_size=latent_dim,
-                                condition_size=condition_size,
-                                n_scales=n_scales, norm='batch',
-                                pool_sizes=pool_size, paddings=padding
+            for pooling in ['max', 'avg']:
+                for pool_size in [1, 2]:
+                    for padding in ['both', None]:
+                        enc = TCN.from_config(
+                            TCN.get_config(
+                                updates=dict(
+                                    input_size=input_size, hidden_sizes=256,
+                                    output_size=latent_dim,
+                                    condition_size=condition_size,
+                                    n_scales=n_scales, norm='batch',
+                                    pooling=pooling, pool_sizes=pool_size,
+                                    paddings=padding
+                                )
                             )
                         )
-                    )
-                    z, pool_indices = enc(x, h)
-                    # self.assertEqual(z.shape, (batch_size, latent_dim, n_frames))
-                    dec = TCN.from_config(
-                        TCN.get_config(
-                            updates=dict(
-                                input_size=latent_dim, hidden_sizes=256,
-                                output_size=input_size,
-                                condition_size=condition_size, transpose=True,
-                                n_scales=n_scales, norm='batch',
-                                pool_sizes=pool_size, paddings=padding
+                        z, pooling_data = enc(x, h)
+                        dec = TCN.from_config(
+                            TCN.get_config(
+                                updates=dict(
+                                    input_size=latent_dim, hidden_sizes=256,
+                                    output_size=input_size,
+                                    condition_size=condition_size,
+                                    transpose=True,
+                                    n_scales=n_scales, norm='batch',
+                                    pooling=pooling, pool_sizes=pool_size,
+                                    paddings=padding
+                                )
                             )
                         )
-                    )
-                    x_hat = dec(z, h, pool_indices=pool_indices[::-1])
-                    self.assertEqual(x_hat.shape, (batch_size, input_size, n_frames))
+                        x_hat = dec(z, h, pooling_data=pooling_data[::-1])
+                        self.assertEqual(
+                            x_hat.shape, (batch_size, input_size, n_frames))
