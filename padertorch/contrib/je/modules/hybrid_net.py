61c9d2474fc54b73b37fb9efafc41f2eca085e48
Janek Ebbers <ebbers@nt.upb.de>
2019-07-11 10:47:14 +0200

    changes in je hybrid net: do not use hybrid cnn

--- padertorch/contrib/je/modules/hybrid_net.py
+++ padertorch/contrib/je/modules/hybrid_net.py
@@ -4,7 +4,7 @@ from paderbox.evaluation.event_detection import positive_class_precisions, \
     lwlrap_from_precisions
 from padertorch.base import Module, Model
 from padertorch.contrib.je.modules.attention import Transformer
-from padertorch.contrib.je.modules.conv import CNN2d, CNN1d, HybridCNN
+from padertorch.contrib.je.modules.conv import CNN1d, CNN2d
 from padertorch.modules.fully_connected import fully_connected_stack
 from padertorch.utils import to_list
 from torch import nn
@@ -17,20 +17,18 @@ class HybridNet(Module):
     """
     >>> hybrid_net = HybridNet.from_config(HybridNet.get_config({\
         'input_size': 80,\
-        'cnn': {\
-            'cnn_2d': {\
-                'in_channels': 1,\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            },\
-            'cnn_1d': {\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            }\
+        'cnn_2d': {\
+            'in_channels': 1,\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
+        },\
+        'cnn_1d': {\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
         },\
         'enc': {'hidden_size': 64},\
         'fcn': {'hidden_size': 32, 'output_size': 10}\
@@ -39,73 +37,96 @@ class HybridNet(Module):
     torch.Size([4, 100, 10])
     """
     def __init__(
-            self, cnn, enc, fcn, pool, *, input_size=None
+            self, cnn_2d, cnn_1d, enc, fcn, pool, *, input_size=None
     ):
         super().__init__()
-        self.cnn = cnn
-        self.enc = enc
-        self.fcn = fcn
-        self.pool = pool
+        self._cnn_2d = cnn_2d
+        self._cnn_1d = cnn_1d
+        self._enc = enc
+        self._fcn = fcn
+        self._pool = pool
         self.input_size = input_size
 
-    def forward(self, x, seq_len=None):
-        if self.cnn is not None:
-            x = self.cnn(x)
+    def cnn_2d(self, x, seq_len=None):
+        if self._cnn_2d is not None:
+            x = self._cnn_2d(x)
             if seq_len is not None:
                 in_shape = [
                     (1024 if self.input_size is None else self.input_size, n)
                     for n in seq_len
-                ] if isinstance(self.cnn, (HybridCNN, CNN2d)) else seq_len
-                out_shape = self.cnn.get_out_shape(in_shape)
-                seq_len = [s[-1] for s in out_shape] if isinstance(self.cnn, CNN2d) else out_shape
-            if isinstance(self.cnn, CNN2d):
-                x = rearrange(x, 'b c f t -> b (c f) t')
-        assert x.dim() == 3  # (B, F, T)
-        if self.enc is None:
+                ]
+                out_shape = self._cnn_2d.get_out_shape(in_shape)
+                seq_len = [s[-1] for s in out_shape]
+
+        if x.dim() != 3:
+            assert x.dim() == 4
+            x = rearrange(x, 'b c f t -> b (c f) t')
+        return x, seq_len
+
+    def cnn_1d(self, x, seq_len=None):
+        if self._cnn_1d is not None:
+            x = self._cnn_1d(x)
+            if seq_len is not None:
+                seq_len = self._cnn_1d.get_out_shape(seq_len)
+        return x, seq_len
+
+    def enc(self, x, seq_len=None):
+        if self._enc is None:
             x = rearrange(x, 'b f t -> b t f')
-        elif isinstance(self.enc, nn.RNNBase):
-            if self.enc.batch_first:
+        elif isinstance(self._enc, nn.RNNBase):
+            if self._enc.batch_first:
                 x = rearrange(x, 'b f t -> b t f')
             else:
                 x = rearrange(x, 'b f t -> t b f')
             if seq_len is not None:
                 x = pack_padded_sequence(
-                    x, seq_len, batch_first=self.enc.batch_first
+                    x, seq_len, batch_first=self._enc.batch_first
                 )
-            x, _ = self.enc(x)
+            x, _ = self._enc(x)
             if seq_len is not None:
-                x = pad_packed_sequence(x, batch_first=self.enc.batch_first)[0]
-            if not self.enc.batch_first:
+                x = pad_packed_sequence(x, batch_first=self._enc.batch_first)[0]
+            if not self._enc.batch_first:
                 x = rearrange(x, 't b f -> b t f')
         # elif isinstance(self.enc, Transformer):
         #     x = self.enc(x.transpose(1, 2))
         else:
             raise NotImplementedError
-        if self.fcn is not None:
-            x = self.fcn(x)
-        if self.pool is not None:
-            x = self.pool(x, seq_len)
+        return x
+
+    def out(self, x, seq_len=None):
+        if self._fcn is not None:
+            x = self._fcn(x)
+        if self._pool is not None:
+            x = self._pool(x, seq_len)
+        return x
+
+    def forward(self, x, seq_len=None):
+        x, seq_len = self.cnn_2d(x, seq_len)
+        x, seq_len = self.cnn_1d(x, seq_len)
+        x = self.enc(x, seq_len)
+        x = self.out(x, seq_len)
         return x
 
     @classmethod
     def finalize_dogmatic_config(cls, config):
-        config['cnn'] = {'factory': HybridCNN}
+        config['cnn_2d'] = {'factory': CNN2d}
+        config['cnn_1d'] = {'factory': CNN1d}
         config['enc'] = {'factory': nn.GRU}
         config['fcn'] = {'factory': fully_connected_stack}
         input_size = config['input_size']
-        if config['cnn'] is not None and input_size is not None:
-            if config['cnn']['factory'] == CNN1d:
-                config['cnn']['in_channels'] = input_size
-                input_size = config['cnn']['out_channels'] \
-                    if config['cnn']['out_channels'] is not None \
-                    else to_list(config['cnn']['hidden_channels'])[-1]
-            elif config['cnn']['factory'] == HybridCNN:
-                config['cnn']['input_size'] = input_size
-                input_size = config['cnn']['cnn_1d']['out_channels'] \
-                    if config['cnn']['cnn_1d']['out_channels'] is not None \
-                    else to_list(config['cnn']['cnn_1d']['hidden_channels'])[-1]
-            else:
-                raise ValueError
+        if config['cnn_2d'] is not None and input_size is not None:
+            cnn_2d = config['cnn_2d']['factory'].from_config(config['cnn_2d'])
+            output_size = cnn_2d.get_out_shape((input_size, 1000))[0]
+            out_channels = cnn_2d.out_channels \
+                if cnn_2d.out_channels is not None \
+                else cnn_2d.hidden_channels[-1]
+            input_size = out_channels * output_size
+
+        if config['cnn_1d'] is not None and input_size is not None:
+            config['cnn_1d']['in_channels'] = input_size
+            input_size = config['cnn_1d']['out_channels'] \
+                if config['cnn_1d']['out_channels'] is not None \
+                else to_list(config['cnn_1d']['hidden_channels'])[-1]
 
         if config['enc'] is not None:
             if config['enc']['factory'] == nn.GRU:
@@ -116,11 +137,11 @@ class HybridNet(Module):
                     'dropout': 0.,
                     'bidirectional': False
                 })
-        elif config['enc']['factory'] == Transformer:
-            config['enc']['norm'] = config['cnn']['norm']
+            elif config['enc']['factory'] == Transformer:
+                config['enc']['norm'] = config['cnn']['norm']
 
-        if input_size is not None:
-            config['enc']['input_size'] = input_size
+            if input_size is not None:
+                config['enc']['input_size'] = input_size
 
         if config['fcn'] is not None:
             config['fcn']['input_size'] = config['enc']['hidden_size']
@@ -133,20 +154,18 @@ class MultinomialClassifier(HybridNet, Model):
         'input_size': 80,\
         'feature_key': 'features',\
         'label_key': 'labels',\
-        'cnn': {\
-            'cnn_2d': {\
-                'in_channels': 1,\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            },\
-            'cnn_1d': {\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            }\
+        'cnn_2d': {\
+            'in_channels': 1,\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
+        },\
+        'cnn_1d': {\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
         },\
         'enc': {'hidden_size': 64},\
         'fcn': {'hidden_size': 32, 'output_size': 10},\
@@ -159,10 +178,10 @@ class MultinomialClassifier(HybridNet, Model):
     >>> review = hybrid_cls.review(inputs, logits)
     """
     def __init__(
-            self, cnn, enc, fcn, pool, *,
+            self, cnn_2d, cnn_1d, enc, fcn, pool, *,
             feature_key, label_key, input_size=None
     ):
-        super().__init__(cnn, enc, fcn, pool, input_size=input_size)
+        super().__init__(cnn_2d, cnn_1d, enc, fcn, pool, input_size=input_size)
         self.feature_key = feature_key
         self.label_key = label_key
 
@@ -189,7 +208,8 @@ class MultinomialClassifier(HybridNet, Model):
             ),
             images=dict(
                 features=inputs[self.feature_key][:3]
-            )
+            ),
+            histograms=dict(labels=labels)
         )
         return summary
 
@@ -218,20 +238,18 @@ class BinaryClassifier(HybridNet, Model):
         'input_size': 80,\
         'feature_key': 'features',\
         'label_key': 'labels',\
-        'cnn': {\
-            'cnn_2d': {\
-                'in_channels': 1,\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            },\
-            'cnn_1d': {\
-                'hidden_channels': 32,\
-                'num_layers': 3,\
-                'out_channels': 16,\
-                'kernel_size': 3\
-            }\
+        'cnn_2d': {\
+            'in_channels': 1,\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
+        },\
+        'cnn_1d': {\
+            'hidden_channels': 32,\
+            'num_layers': 3,\
+            'out_channels': 16,\
+            'kernel_size': 3\
         },\
         'enc': {'hidden_size': 64},\
         'fcn': {'hidden_size': 32, 'output_size': 10},\
@@ -244,10 +262,10 @@ class BinaryClassifier(HybridNet, Model):
     >>> review = hybrid_cls.review(inputs, logits)
     """
     def __init__(
-            self, cnn, enc, fcn, pool, output_activation, *,
+            self, cnn_2d, cnn_1d, enc, fcn, pool, output_activation, *,
             feature_key, label_key, input_size=None, decision_boundary=.5
     ):
-        super().__init__(cnn, enc, fcn, pool, input_size=input_size)
+        super().__init__(cnn_2d, cnn_1d, enc, fcn, pool, input_size=input_size)
         self.output_activation = output_activation
         self.feature_key = feature_key
         self.label_key = label_key
