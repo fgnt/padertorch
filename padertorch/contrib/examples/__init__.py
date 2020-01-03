9045238cc214b719091d8161e2328da32fc8d389
jensheit <heitkaemper@nt.upb.de>
2020-01-03 14:25:17 +0100

    fix database import check

--- padertorch/contrib/examples/__init__.py
+++ padertorch/contrib/examples/__init__.py
@@ -1,9 +1,9 @@
 try:
-    import paderbox.database
+    import padercontrib.database
 except:
     raise NotImplementedError(
-        "These examples are depending on our internal database structure"
-        "at the moment."
+        "These examples are depending on our internal database structure "
+        "at the moment. "
         "Trying to execute them anyway may take considerable "
         "effort on your behalf."
 )
