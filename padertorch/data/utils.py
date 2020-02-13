979fc83d03456b1b1ffff5c655f4998c96867e0c
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-02-13 16:48:11 +0100

    rewrote collate_fn and added dataclass support

--- padertorch/data/utils.py
+++ padertorch/data/utils.py
@@ -19,32 +19,47 @@ def pad_tensor(vec, pad, axis):
 
 
 def collate_fn(batch):
-    """Moves list inside of dict recursively.
+    """Moves list inside of dict/dataclass recursively.
 
-    Can be used as input to batch iterator.
+    Can be used as map after batching of an dataset:
+        `dataset.batch(...).map(collate_fn)`
 
     Args:
-        batch:
+        batch: list of examples
 
     Returns:
 
-    """
-    def nested_batching(value, key, nested_batch):
-        # recursively nesting the batch
-        if isinstance(value, dict):
-            if key not in nested_batch:
-                nested_batch[key] = dict()
-            return {k: nested_batching(v, k, nested_batch[key])
-                    for k, v in value.items()}
-        else:
-            if key not in nested_batch:
-                nested_batch[key] = []
-            nested_batch[key].append(value)
-        return nested_batch[key]
+    >>> batch = [{'a':  1}, {'a': 2}]
+    >>> collate_fn(batch)
+    {'a': [1, 2]}
 
-    nested_batch = {}
-    for elem in batch:
-        assert isinstance(elem, dict)
-        nested_batch = {key: nested_batching(value, key, nested_batch)
-                        for key, value in elem.items()}
-    return nested_batch
+    >>> batch = [{'a':  {'b': [1, 2]}}, {'a': {'b': [3, 4]}}]
+    >>> collate_fn(batch)
+    {'a': {'b': [[1, 2], [3, 4]]}}
+
+    >>> import dataclasses
+    >>> Point = dataclasses.make_dataclass('Point', ['x', 'y'])
+    >>> batch = [Point(1, 2), Point(3, 4)]
+    >>> batch
+    [Point(x=1, y=2), Point(x=3, y=4)]
+    >>> collate_fn(batch)
+    Point(x=[1, 3], y=[2, 4])
+    """
+    assert isinstance(batch, (tuple, list)), (type(batch), batch)
+
+    if isinstance(batch[0], dict):
+        for b in batch[1:]:
+            assert batch[0].keys() == b.keys(), batch
+        return batch[0].__class__({
+            k: (collate_fn([b[k] for b in batch]))
+            for k in batch[0]
+        })
+    elif hasattr(batch[0], '__dataclass_fields__'):
+        for b in batch[1:]:
+            assert batch[0].__dataclass_fields__ == b.__dataclass_fields__, batch
+        return batch[0].__class__(**{
+            k: (collate_fn([getattr(b, k) for b in batch]))
+            for k in batch[0].__dataclass_fields__
+        })
+    else:
+        return batch
