83607b7481630c7b595dadac463ebe1bfa85594e
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-02-13 16:57:30 +0100

    collate_fn: keep outer type

--- padertorch/data/utils.py
+++ padertorch/data/utils.py
@@ -29,11 +29,13 @@ def collate_fn(batch):
 
     Returns:
 
-    >>> batch = [{'a':  1}, {'a': 2}]
+    >>> batch = [{'a': 1}, {'a': 2}]
     >>> collate_fn(batch)
     {'a': [1, 2]}
+    >>> collate_fn(tuple(batch))
+    {'a': (1, 2)}
 
-    >>> batch = [{'a':  {'b': [1, 2]}}, {'a': {'b': [3, 4]}}]
+    >>> batch = [{'a': {'b': [1, 2]}}, {'a': {'b': [3, 4]}}]
     >>> collate_fn(batch)
     {'a': {'b': [[1, 2], [3, 4]]}}
 
@@ -44,6 +46,8 @@ def collate_fn(batch):
     [Point(x=1, y=2), Point(x=3, y=4)]
     >>> collate_fn(batch)
     Point(x=[1, 3], y=[2, 4])
+    >>> collate_fn(tuple(batch))
+    Point(x=(1, 3), y=(2, 4))
     """
     assert isinstance(batch, (tuple, list)), (type(batch), batch)
 
@@ -51,14 +55,14 @@ def collate_fn(batch):
         for b in batch[1:]:
             assert batch[0].keys() == b.keys(), batch
         return batch[0].__class__({
-            k: (collate_fn([b[k] for b in batch]))
+            k: (collate_fn(batch.__class__([b[k] for b in batch])))
             for k in batch[0]
         })
     elif hasattr(batch[0], '__dataclass_fields__'):
         for b in batch[1:]:
             assert batch[0].__dataclass_fields__ == b.__dataclass_fields__, batch
         return batch[0].__class__(**{
-            k: (collate_fn([getattr(b, k) for b in batch]))
+            k: (collate_fn(batch.__class__([getattr(b, k) for b in batch])))
             for k in batch[0].__dataclass_fields__
         })
     else:
