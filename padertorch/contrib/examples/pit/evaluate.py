f7e816a42c9830ad57ac5f21d3b6a621dde1ddf3
TCord <49722035+TCord@users.noreply.github.com>
2020-05-19 14:37:08 +0200

    Removed redundant no_grad()

--- padertorch/contrib/examples/pit/evaluate.py
+++ padertorch/contrib/examples/pit/evaluate.py
@@ -130,39 +130,38 @@ def main(_run, batch_size, datasets, debug, experiment_dir, database_json):
     db = JsonDatabase(json_path=database_json)
 
     model.eval()
-    with torch.no_grad():
-        summary = defaultdict(dict)
-        for dataset in datasets:
-            iterable = prepare_iterable(
-                db, dataset, batch_size,
-                return_keys=None,
-                prefetch=False,
+    summary = defaultdict(dict)
+    for dataset in datasets:
+        iterable = prepare_iterable(
+            db, dataset, batch_size,
+            return_keys=None,
+            prefetch=False,
+        )
+
+        for batch in split_managed(iterable, is_indexable=False,
+                                   progress_bar=True,
+                                   allow_single_worker=debug
+                                   ):
+            entry = dict()
+            model_output = model(pt.data.example_to_device(batch))
+
+            example_id = batch['example_id'][0]
+            s = batch['s'][0]
+            Y = batch['Y'][0]
+            mask = model_output[0].numpy()
+
+            Z = mask * Y[:, None, :]
+            z = istft(
+                einops.rearrange(Z, "t k f -> k t f"),
+                size=512, shift=128
             )
 
-            for batch in split_managed(iterable, is_indexable=False,
-                                       progress_bar=True,
-                                       allow_single_worker=debug
-                                       ):
-                entry = dict()
-                model_output = model(pt.data.example_to_device(batch))
+            s = s[:, :z.shape[1]]
+            z = z[:, :s.shape[1]]
+            entry['mir_eval'] \
+                = pb_bss.evaluation.mir_eval_sources(s, z, return_dict=True)
 
-                example_id = batch['example_id'][0]
-                s = batch['s'][0]
-                Y = batch['Y'][0]
-                mask = model_output[0].numpy()
-
-                Z = mask * Y[:, None, :]
-                z = istft(
-                    einops.rearrange(Z, "t k f -> k t f"),
-                    size=512, shift=128
-                )
-
-                s = s[:, :z.shape[1]]
-                z = z[:, :s.shape[1]]
-                entry['mir_eval'] \
-                    = pb_bss.evaluation.mir_eval_sources(s, z, return_dict=True)
-
-                summary[dataset][example_id] = entry
+        summary[dataset][example_id] = entry
 
     summary_list = COMM.gather(summary, root=MASTER)
 
