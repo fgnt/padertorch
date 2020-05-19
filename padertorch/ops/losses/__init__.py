9d9bfb1b6e417f1a1438acb2004d7545d559e1a1
jensheit <heitkaemper@nt.upb.de>
2020-05-19 11:20:39 +0200

    update losses init after changes

--- padertorch/ops/losses/__init__.py
+++ padertorch/ops/losses/__init__.py
@@ -1,5 +1,9 @@
-from . import loss
+from . import classification
 from . import regression
+from . import source_separation
+from . import kl_divergence
 
-from .loss import *
+from .classification import *
 from .regression import *
+from .source_separation import *
+from .kl_divergence import *
