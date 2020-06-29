56a635ea89e6695d095849416e48e60a043a82fc
Frederik Rautenberg <rautenberg@mail.upb.de>
2020-06-29 11:11:48 +0200

    correct github comments

--- padertorch/contrib/examples/vae_mnist/mnist_vae_example.py
+++ padertorch/contrib/examples/vae_mnist/mnist_vae_example.py
@@ -9,58 +9,62 @@ Other options for sacred are available, refer to the config() function.
 """
 import os
 
-import padertorch
+import padertorch as pt
 import padertorch.train.optimizer as pt_opt
 
 import sacred
 
 import torch
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
 
 from einops import rearrange
+
+from padertorch.contrib.je.data.transforms import Collate
+from padertorch.ops.losses.kl_divergence import gaussian_kl_divergence
+from padertorch.ops.mappings import ACTIVATION_FN_MAP
+from padertorch.testing.test_db import MnistDatabase
+
+from torch.distributions import Normal
 
 ex = sacred.Experiment()
 
 
-def get_dataset(batch_size=128):
+def get_dataset(batch_size=10):
     """
-    load MNIST dataset and store it in ./data
+    load MNIST train and test dataset
 
     Args:
         batch_size (int):
 
     Returns:
-        trainloader and testloader
+        train_set and test_set
 
     """
-    tf = transforms.Compose([transforms.ToTensor()])
+    db = MnistDatabase()
+    train_set = db.get_dataset('train')
+    test_set = db.get_dataset('test')
 
-    # Get Datasets
-    trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                          download=True, transform=tf)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
+    training_data = prepare_dataset(train_set, batch_size=batch_size, training=True)
+    test_data = prepare_dataset(test_set, batch_size=batch_size, training=False)
 
-    testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                         download=True, transform=tf)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-                                             shuffle=False, num_workers=2)
-
-    return trainloader, testloader
+    return training_data, test_data
 
 
-class PadertorchModel(padertorch.base.Model):
-    MSE_loss = nn.MSELoss()
+def prepare_dataset(dataset, batch_size=10, training=False):
+    if training:
+        dataset = dataset.shuffle(reshuffle=True)
+    return dataset.prefetch(
+        num_workers=8, buffer_size=10*batch_size
+    ).batch(batch_size=batch_size).map(Collate())
 
-    def __init__(self, net):
+
+class VAEModel(pt.Model):
+    def __init__(self, activation_function, hidden_channels):
         super().__init__()
-        self.net = net
+        self.net = VAE(activation_function, hidden_channels)
 
     def forward(self, inputs):
-        images = inputs[0]
-        out, mu, std = self.net(images)
+        out, mu, std = self.net(inputs['image'][:, None, :, :])
 
         return dict(
             prediction=out,
@@ -69,14 +73,18 @@ class PadertorchModel(padertorch.base.Model):
         )
 
     def review(self, inputs, outputs):
-        images = rearrange(inputs[0], 'b c x y -> b (c x y)')
+        images = rearrange(inputs['image'][:, None, :, :], 'b c x y -> b (c x y)')
         prediction = rearrange(outputs['prediction'], 'b c x y -> b (c x y)')
 
-        MSE = self.MSE_loss(prediction, images)  # reconstruction error
-        # Kullback-Leibler divergence
-        KLD = -0.5 * torch.sum(1 + outputs['std'] - outputs['mean'].pow(2) - outputs['std'].exp())
+        mse = nn.functional.mse_loss(prediction, images)  # reconstruction error
 
-        loss = MSE + KLD
+        q = Normal(loc=outputs['mean'], scale=outputs['std'])  # posterior distribution
+        mean = torch.zeros(outputs['mean'].shape[-1])
+        std = torch.ones(outputs['std'].shape[-1])
+        p = Normal(loc=mean, scale=std)  # prior distribution zero mean and unit variance
+        kld = gaussian_kl_divergence(q=q, p=p)  # regularisation error
+
+        loss = mse + torch.sum(kld)
 
         return dict(
             loss=loss,
@@ -86,39 +94,38 @@ class PadertorchModel(padertorch.base.Model):
 class VAE(nn.Module):
     """ A simple Variational Autoencoder"""
 
-    def __init__(self):
-        super(VAE, self).__init__()
-        self.kernel_size = 4
-
+    def __init__(self, activation_function, hidden_channels):
+        super().__init__()
+        kernel_size = 4
+        stride = int(kernel_size/2)
         # encoder
-        self.conv_1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=self.kernel_size,
-                                      stride=2)
-        self.conv_2 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=self.kernel_size,
-                                      stride=2)
-        self.conv_3 = torch.nn.Conv2d(in_channels=64, out_channels=1, kernel_size=self.kernel_size,
-                                      stride=1)
+        self.conv_encoder = nn.Sequential(
+            torch.nn.Conv2d(in_channels=1, out_channels=hidden_channels, kernel_size=kernel_size, stride=stride),
+            activation_function(),
+            torch.nn.Conv2d(in_channels=hidden_channels, out_channels=hidden_channels, kernel_size=kernel_size,
+                            stride=stride),
+            activation_function(),
+            torch.nn.Conv2d(in_channels=hidden_channels, out_channels=1, kernel_size=kernel_size, stride=1),
+            activation_function(),
+        )
         self.linear_1 = torch.nn.Linear(in_features=2*2, out_features=1)
         self.linear_2 = torch.nn.Linear(in_features=2*2, out_features=1)
-        self.lrelu = torch.nn.LeakyReLU()
 
         # decoder
+        self.transp_conv_decoder = nn.Sequential(
+            torch.nn.ConvTranspose2d(in_channels=1, out_channels=hidden_channels, kernel_size=kernel_size,
+                                     stride=stride),
+            activation_function(),
+            torch.nn.ConvTranspose2d(in_channels=hidden_channels, out_channels=hidden_channels,
+                                     kernel_size=kernel_size, stride=stride),
+            activation_function(),
+            torch.nn.ConvTranspose2d(in_channels=hidden_channels, out_channels=1, kernel_size=kernel_size,
+                                     stride=stride),
+        )
         self.linear_3 = torch.nn.Linear(in_features=1, out_features=2*2)
 
-        self.transconv_1 = torch.nn.ConvTranspose2d(in_channels=1, out_channels=64, kernel_size=self.kernel_size,
-                                                   stride=2)
-        self.transconv_2 = torch.nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=self.kernel_size,
-                                                   stride=2)
-        self.transconv_3 = torch.nn.ConvTranspose2d(in_channels=64, out_channels=1, kernel_size=self.kernel_size,
-                                                   stride=2)
-        self.relu = torch.nn.ReLU()
-
     def encode(self, x):
-        x = self.conv_1(x)
-        x = self.lrelu(x)
-        x = self.conv_2(x)
-        x = self.lrelu(x)
-        x = self.conv_3(x)
-        x = self.lrelu(x)
+        x = self.conv_encoder(x)
         x = rearrange(x, 'b c x y -> b (c x y)')
         mu = self.linear_1(x)  # mean value
         std = 0.5 * self.linear_2(x)  # standard deviation
@@ -129,12 +136,7 @@ class VAE(nn.Module):
         x = self.linear_3(x)
         x = rearrange(x, 'b (x y) -> b x y', y=2, x=2)
         x = x[:, None, :, :]
-        x = self.transconv_1(x)
-        x = self.relu(x)
-        x = self.transconv_2(x)
-        x = self.relu(x)
-        x = self.transconv_3(x)
-        return x
+        return self.transp_conv_decoder(x)
 
     def forward(self, input):
         x, mu, std = self.encode(input)
@@ -145,13 +147,18 @@ class VAE(nn.Module):
 @ex.config
 def config():
     epochs = 2          # Use resume=True to train for more epochs
-    batch_size = 128
+    batch_size = 10
     resume = False      # PT: Continue from checkpoints
+
+    assert os.environ.get('STORAGE_ROOT') is not None, 'STORAGE_ROOT is not defined in the environment variables'
     logfilepath = os.environ['STORAGE_ROOT'] + '/mnist'  # PT creates log files
+
+    activation_function = ACTIVATION_FN_MAP['relu']  # activation function after convolutional layers
+    hidden_channels = 64
 
 
 @ex.automain
-def main(epochs, batch_size, resume, logfilepath):
+def main(activation_function, batch_size, epochs, hidden_channels, resume, logfilepath):
     """
     Example Training for a Variational Autoencoder uses SGD for optimation
 
@@ -171,15 +178,12 @@ def main(epochs, batch_size, resume, logfilepath):
                     ├── events.out.tfevents.1548851867.ntsim5
     """
     # load dataset
-    trainloader, testloader = get_dataset(batch_size=batch_size)
-
-    # Setup net and model
-    net = VAE()
+    train_set, test_set = get_dataset(batch_size=batch_size)
 
     # load model
-    model = PadertorchModel(net)
+    model = VAEModel(activation_function, hidden_channels)
 
-    trainer = padertorch.trainer.Trainer(
+    trainer = pt.trainer.Trainer(
         model=model,
         storage_dir=logfilepath,
         optimizer=pt_opt.SGD(),
@@ -187,9 +191,6 @@ def main(epochs, batch_size, resume, logfilepath):
         #checkpoint_trigger=(5000, 'iteration'),
         summary_trigger=(1000, 'iteration'),
     )
-    trainer.register_validation_hook(validation_iterator=testloader)
-    try:
-        trainer.train(trainloader, resume=resume)
-    except Exception:
-        print('#' * 1000)
-        raise
+
+    trainer.register_validation_hook(validation_iterator=test_set)
+    trainer.train(train_set, resume=resume)
