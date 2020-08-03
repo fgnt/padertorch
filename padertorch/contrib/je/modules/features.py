fix Normalization imports in contrib

padertorch/contrib/je/modules/features.py
@@ -8,7 +8,7 @@ from padertorch.contrib.je.modules.augment import (
     Scale, Resample, GaussianBlur2d, Mask, Noise,
     LogUniformSampler, TruncExponentialSampler, LogTruncNormalSampler
 )
-from padertorch.contrib.je.modules.norm import Norm
+from padertorch.modules.normalization import Normalization
 from torch import nn
 from paderbox.transform.module_fbank import get_fbanks
 
@@ -38,7 +38,7 @@ class NormalizedLogMelExtractor(nn.Module):
         )
         self.add_deltas = add_deltas
         self.add_delta_deltas = add_delta_deltas
-        self.norm = Norm(
+        self.norm = Normalization(
             data_format='bcft',
             shape=(None, 1 + add_deltas + add_delta_deltas, n_mels, None),
             statistics_axis=mel_norm_statistics_axis,
@@ -47,7 +47,6 @@ class NormalizedLogMelExtractor(nn.Module):
             eps=mel_norm_eps,
             independent_axis=None,
             momentum=None,
-            interpolation_factor=1.,
         )
 
         # augmentation
@@ -109,7 +108,7 @@ class NormalizedLogMelExtractor(nn.Module):
                     delta_deltas = compute_deltas(deltas)
                     x = torch.cat((x, delta_deltas), dim=1)
 
-            x = self.norm(x, seq_len=seq_len)
+            x = self.norm(x, sequence_lengths=seq_len)
 
             if self.time_masking is not None:
                 x = self.time_masking(x, seq_len=seq_len)
@@ -201,7 +200,7 @@ class MelTransform(Module):
         return nn.ReLU()(fbanks)
 
     def forward(self, x):
-        x = x @ self.get_fbanks(x)
+        x = x.matmul(self.get_fbanks(x))
         if self.log:
             x = torch.log(x + self.eps)
         return x

padertorch/contrib/je/modules/rnn.py
@@ -1,7 +1,7 @@
 from torch import nn
 import torch
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from padertorch.contrib.je.modules.global_pooling import compute_mask
+from padertorch.ops.sequence.mask import compute_mask
 
 
 class RNN(nn.Module):

padertorch/contrib/je/modules/transformer.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from padertorch.base import Module
 from padertorch.ops.mappings import ACTIVATION_FN_MAP
-from padertorch.contrib.je.modules.norm import Norm
-from padertorch.contrib.je.modules.global_pooling import compute_mask
+from padertorch.modules.normalization import Normalization
+from padertorch.ops.sequence.mask import compute_mask
 
 
 def scaled_dot_product_attention(q, k, v, seq_len=None, bidirectional=False):
@@ -29,7 +29,7 @@ def scaled_dot_product_attention(q, k, v, seq_len=None, bidirectional=False):
         mask = get_causal_mask(y)
         y = y + torch.log((mask > 0).float())
     elif seq_len is not None:
-        mask = compute_mask(y, seq_len, seq_axis=-1)
+        mask = compute_mask(y, seq_len, sequence_axis=-1)
         y = y + torch.log((mask > 0).float())
     return torch.softmax(y, dim=-1)@v
 
@@ -112,14 +112,14 @@ class TransformerBlock(Module):
             # ToDo: where is the difference between layer norm and instance norm?
         else:
             raise ValueError(f'{norm} normalization not known.')
-        self.self_attention_norm = Norm(**norm_kwargs)
-        self.output_norm = Norm(**norm_kwargs)
+        self.self_attention_norm = Normalization(**norm_kwargs)
+        self.output_norm = Normalization(**norm_kwargs)
 
         if cross_attention:
             self.multi_head_cross_attention = MultiHeadAttention(
                 hidden_size, hidden_size, num_heads, bidirectional=True
             )
-            self.cross_attention_norm = Norm(**norm_kwargs)
+            self.cross_attention_norm = Normalization(**norm_kwargs)
 
     def forward(self, x, v=None, seq_len_x=None, seq_len_v=None, state=None):
         x_ = x if state is None else torch.cat([state, x], 1)