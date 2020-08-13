9df4d27103cda42e5071e5b83723ba8292a5edf3
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-08-13 17:14:38 +0200

    update docstring

--- padertorch/io.py
+++ padertorch/io.py
@@ -10,24 +10,65 @@ import padertorch as pt
 
 def get_new_folder(
         basedir,
-        try_id=None,
-        dry_run=False,
-        consider_mpi=False,
+        *,
         id_type='index',
         mkdir=True,
+        try_id=None,
         force_suffix=False,
+        consider_mpi=False,
+        dry_run=False,
 ):
     """
 
     Args:
         basedir:
+            The new folder will be inside this directory
+        id_type:
+            The id type that is used for the folder name.
+             - index:
+                The largest index in basedir + 1.
+                e.g.: '1', '2', ...
+             - time: A timestamp with the format %Y-%m-%d-%H-%M-%S
+                e.g. '2020-08-13-17-02-57'
+        mkdir:
+            Creates the dir and makes the program process/thread safe.
+            Note this option ensures that you don't get a
+            conflict between two concurrent calls of get_new_folder.
+            Example:
+                You launch several times your programs and each should get
+                another folder (e.g. hyperparameter search). When inspecting
+                basedir maybe some recognize they can use '2' as sub folder.
+                This option ensures, that only one program gets the '2' and the
+                remaining programs search for another free id.
         try_id:
-        mkdir: Enables thread safety
-        dry_run: Per default also creates the directory to be thread safe.
+            When not None use this as prefix for the new folder name.
+            First try f'{try_id}' before continuing with f'{try_id}_2'.
+        force_suffix:
+            If True, use f'{try_id}_1' instead of f'{try_id}' as first try when
+            try_id is not None.
+        consider_mpi:
+            If True, only search on one mpi process for the folder and
+            distribute the folder name.
+            When using mpi with this option disables the following can/will
+            happen
+             - When mkdir is True every process will get another folder.
+               i.e. each process has a folder just for this process.
+             - When mkdir is False it is likely that all get the same folder,
+               but this is not guarantied. Never use mpi with consider_mpi and
+               mkdir disabled.
+        dry_run:
+            Similar to mkdir. When true, disable mkdir and print the folder
+            name.
 
     Returns:
+        pathlib.Path of the new folder
 
+
+    >>> get_new_folder('/', dry_run=True)  # root folder usually contain no digits
+    dry_run: "os.mkdir(/1)"
+    PosixPath('/1')
     """
+
     if consider_mpi:
         import dlp_mpi
         if dlp_mpi.IS_MASTER:
