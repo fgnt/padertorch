0f9186909962c41659924e6f297658b3f28dbf99
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-08-18 14:21:36 +0200

    tbx_utils: add color option to mask_to_image and add option to disable batch_first argument (i.e. batch_first == None)

--- padertorch/summary/tbx_utils.py
+++ padertorch/summary/tbx_utils.py
@@ -18,17 +18,24 @@ def _remove_batch_axis(array, batch_first, ndim=2):
     if array.ndim == ndim:
         pass
     elif array.ndim == ndim + 1:
-        if batch_first:
+        if batch_first is True:
             array = array[0]
-        else:
+        elif batch_first is False:
             array = array[:, 0]
+        elif batch_first is None:
+            raise ValueError(
+                'Remove "remove batch axis" is disabled '
+                '(i.e. batch_first=None)\n'
+                f'It looks like the array still has a batch axis.\n'
+                f'Shape: {array.shape}'
+            )
     else:
         raise ValueError(f'Either the signal has ndim {ndim} or {ndim + 1}',
                          array.shape)
     return array
 
 
-def mask_to_image(mask, batch_first=False):
+def mask_to_image(mask, batch_first=False, color=None):
     """
     For more details of the output shape, see the tensorboardx docs
     Args:
@@ -45,7 +52,7 @@ def mask_to_image(mask, batch_first=False):
 
     image = _remove_batch_axis(image, batch_first=batch_first)
 
-    return image[None].transpose(0, 2, 1)[:, ::-1]
+    return _colorize(image, color).transpose(0, 2, 1)[:, ::-1]
 
 
 def stft_to_image(signal, batch_first=False, color='viridis'):
@@ -66,6 +73,54 @@ def stft_to_image(signal, batch_first=False, color='viridis'):
 
 
 _spectrogram_to_image_cmap = {}
+
+
+def _colorize(image, color):
+    """
+    >>> i = np.arange(15).reshape([3, 5])
+    >>> _colorize(i, True).shape
+    (4, 3, 5)
+    >>> _colorize(i, None).shape
+    (1, 3, 5)
+    >>> i = np.arange(6).reshape([2, 3])
+    >>> _colorize(i, True)
+    array([[[0.267004, 0.26851 , 0.269944],
+            [0.271305, 0.272594, 0.273809]],
+    <BLANKLINE>
+           [[0.004874, 0.009605, 0.014625],
+            [0.019942, 0.025563, 0.031497]],
+    <BLANKLINE>
+           [[0.329415, 0.335427, 0.341379],
+            [0.347269, 0.353093, 0.358853]],
+    <BLANKLINE>
+           [[1.      , 1.      , 1.      ],
+            [1.      , 1.      , 1.      ]]])
+    >>> _colorize(i, None)
+    array([[[0, 1, 2],
+            [3, 4, 5]]])
+    """
+    if color is None:
+        return image[None, :, :]
+    else:
+        if color is True:
+            color = 'viridis'
+        try:
+            cmap = _spectrogram_to_image_cmap[color]
+        except KeyError:
+            try:
+                import matplotlib.pyplot as plt
+                cmap = plt.cm.get_cmap(color)
+                _spectrogram_to_image_cmap[color] = cmap
+            except ImportError:
+                from warnings import warn
+                gray_scale = lambda x: x.transpose(1, 0)[None, ::-1, :]
+                warn('Since matplotlib is not installed, all images are '
+                     'switched to grey scale')
+                _spectrogram_to_image_cmap[color] = gray_scale
+                cmap = gray_scale
+                # gray image
+                # return gray_scale(image)
+        return np.moveaxis(cmap(image), -1, 0)
 
 
 def spectrogram_to_image(signal, batch_first=False, color='viridis'):
