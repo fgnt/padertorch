60aceabaaddb73df7e5ab50d49aecd947db2ae43
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-08-18 16:01:00 +0200

    tbx_utils: remove duplicate code

--- padertorch/summary/tbx_utils.py
+++ padertorch/summary/tbx_utils.py
@@ -172,27 +172,7 @@ def spectrogram_to_image(
 
     signal = (signal * 255).astype(np.uint8)
 
-    if color is not None:
-        try:
-            cmap = _spectrogram_to_image_cmap[color]
-        except KeyError:
-            try:
-                import matplotlib.pyplot as plt
-                cmap = plt.cm.get_cmap(color)
-                _spectrogram_to_image_cmap[color] = cmap
-            except ImportError:
-                from warnings import warn
-                gray_scale = lambda x: x.transpose(1, 0)[None, ::-1, :]
-                warn('Since matplotlib is not installed, all images are '
-                     'switched to grey scale')
-                _spectrogram_to_image_cmap[color] = gray_scale
-                # gray image
-                return gray_scale(signal)
-
-        return cmap(signal).transpose(2, 1, 0)[:, ::-1, :]
-    else:
-        # gray image
-        return signal.transpose(1, 0)[None, ::-1, :]
+    return _colorize(_apply_origin(signal.T, origin=origin), color)
 
 
 def audio(signal, sampling_rate: int = 16000, batch_first=False,
