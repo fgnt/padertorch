e77d6fcd295aef25d0b6bc28ecf05c08d1a7e387
jensheit <heitkaemper@nt.upb.de>
2020-09-02 08:43:58 +0200

    remove dependencies from padercontrib from train and add sacred config to allow to specify a json

--- padertorch/contrib/examples/speech_enhancement/simple_mask_estimator/train.py
+++ padertorch/contrib/examples/speech_enhancement/simple_mask_estimator/train.py
@@ -2,71 +2,75 @@
 Very simple training script for a mask estimator.
 Saves checkpoints and summaries to $STORAGE_ROOT/speech_enhancement/simple_mask_estimator_{id}
 may be called with:
-python -m padertorch.contrib.examples.speech_enhancement.simple_mask_estimator.train
+python -m padertorch.contrib.examples.speech_enhancement.simple_mask_estimator.train with database_json=/path/to/json
 """
+
+from pathlib import Path
 
 import numpy as np
 import paderbox as pb
-import padercontrib.database.keys as K
 import padertorch as pt
-from sacred import Experiment
-from padercontrib.database import JsonAudioDatabase
-from padercontrib.database.chime import Chime3
-from padercontrib.database.iterator import AudioReader
+from lazy_dataset.database import JsonDatabase
 from pb_bss.extraction.mask_module import biased_binary_mask
-from padertorch.contrib.examples.speech_enhancement.simple_mask_estimator import SimpleMaskEstimator
+from sacred import Experiment, observers
+
+from .model import SimpleMaskEstimator
 
 ex = Experiment('Simple Mask Estimator')
 
 
-def change_example_structure(example):
-    stft = pb.transform.stft
-    audio_data = example[K.AUDIO_DATA]
-    net_input = dict()
-    net_input['observation_stft'] = stft(
-        audio_data[K.OBSERVATION]).astype(np.complex64)
-    net_input['observation_abs'] = np.abs(
-        net_input['observation_stft']).astype(np.float32)
-    speech_image = stft(audio_data[K.SPEECH_IMAGE])
-    noise_image = stft(audio_data[K.NOISE_IMAGE])
-    target_mask, noise_mask = biased_binary_mask(
-        np.stack([speech_image, noise_image], axis=0)
+@ex.config
+def config():
+    storage_dir = pt.io.get_new_storage_dir(
+        'speech_enhancement', prefix='simple_mask_estimator')
+    database_json = None
+    assert database_json is not None, (
+        'You have to specify a path to a json describing your database,'
+        'use "with database_json=/Path/To/Json" as suffix to your call'
     )
+    assert Path(database_json).exists(), database_json
+    ex.observers.append(observers.FileStorageObserver(
+        Path(storage_dir).expanduser().resolve() / 'sacred')
+    )
+
+
+def prepare_data(example):
+    stft = pb.transform.STFT(shift=256, size=1024)
+    net_input = dict()
+    audio_data = dict()
+    for key in ['observation', 'speech_image', 'noise_image']:
+        audio_data[key] = stft(np.array([
+            pb.io.load_audio(audio) for audio in example['audio_path'][key]]))
+    net_input['observation_abs'] = np.abs(
+        audio_data['observation']).astype(np.float32)
+    target_mask, noise_mask = biased_binary_mask(np.stack(
+        [audio_data['speech_image'], audio_data['noise_image']], axis=0
+    ))
     net_input['speech_mask_target'] = target_mask.astype(np.float32)
     net_input['noise_mask_target'] = noise_mask.astype(np.float32)
     return net_input
 
 
-def get_train_dataset(database: JsonAudioDatabase):
-    # AudioReader is a specialized function to read audio organized
-    # in a json as described in pb.database.database
-    audio_reader = AudioReader(audio_keys=[
-        K.OBSERVATION, K.NOISE_IMAGE, K.SPEECH_IMAGE
-    ])
-    train_ds = database.get_dataset_train()
+def get_train_dataset(database: JsonDatabase):
+    train_ds = database.get_dataset('tr05_simu')
     return (train_ds
-            .map(audio_reader)
-            .map(change_example_structure)
+            .map(prepare_data)
             .prefetch(num_workers=4, buffer_size=4))
 
 
-def get_validation_dataset(database: JsonAudioDatabase):
+def get_validation_dataset(database: JsonDatabase):
     # AudioReader is a specialized function to read audio organized
     # in a json as described in pb.database.database
-    audio_reader = AudioReader(audio_keys=[
-        K.OBSERVATION, K.NOISE_IMAGE, K.SPEECH_IMAGE
-    ])
-    val_iterator = database.get_dataset_validation()
-    return val_iterator.map(audio_reader) \
-        .map(change_example_structure) \
+    val_iterator = database.get_dataset('dt05_simu')
+    return val_iterator.map(prepare_data) \
         .prefetch(num_workers=4, buffer_size=4)
 
 
 @ex.command
-def test_run():
+def test_run(chime3_json_path):
     model = SimpleMaskEstimator(513)
     print(f'Simple training for the following model: {model}')
-    database = Chime3()
+    database = JsonDatabase(chime3_json_path)
     train_dataset = get_train_dataset(database)
     validation_dataset = get_validation_dataset(database)
     trainer = pt.train.trainer.Trainer(
@@ -77,12 +81,10 @@ def test_run():
 
 
 @ex.automain
-def train():
-    storage_dir = pt.io.get_new_storage_dir(
-        'speech_enhancement', prefix='simple_mask_estimator')
+def train(storage_dir, database_json):
     model = SimpleMaskEstimator(513)
     print(f'Simple training for the following model: {model}')
-    database = Chime3()
+    database = JsonDatabase(database_json)
     train_dataset = get_train_dataset(database)
     validation_dataset = get_validation_dataset(database)
     trainer = pt.Trainer(model, storage_dir,
