3a1b82e1dd101fe67e1f3c9381f225bda330e3e3
Christoph Boeddeker <boeddeker@users.noreply.github.com>
2020-09-15 18:12:33 +0200

    add pb_bss as test dependency

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test = [
     'protobuf3_to_dict',
     'torchvision',
     'matplotlib',  # padertorch.summary.tbx_utils use mpl for colorize
-    'pb_bss',
+    'pb_bss @ git+http://github.com/fgnt/pb_bss',
 ]
 
 setup(
