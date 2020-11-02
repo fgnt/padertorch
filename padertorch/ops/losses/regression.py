977c2327b22213be070bc9782cf0724dc10ed0ed
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2020-11-02 18:59:19 +0100

    fix regression functions: They ignored the reduce agrument

--- padertorch/ops/losses/regression.py
+++ padertorch/ops/losses/regression.py
@@ -9,20 +9,20 @@ def _get_scaling_factor(targets, estimates):
     ), -1) / torch.norm(targets, dim=-1, keepdim=True) ** 2
 
 
-def _reduce(array, reduce):
-    if reduce is None or reduce == 'none':
+def _reduce(array, reduction):
+    if reduction is None or reduction == 'none':
         return array
-    if reduce == 'sum':
+    if reduction == 'sum':
         return torch.sum(array)
-    elif reduce == 'mean':
+    elif reduction == 'mean':
         return torch.mean(array)
     else:
-        raise ValueError(f'Unknown reduce: {reduce}. Choose from "sum", '
+        raise ValueError(f'Unknown reduce: {reduction}. Choose from "sum", '
                          f'"mean".')
 
 
 def log_mse_loss(estimate: torch.Tensor, target: torch.Tensor,
-                 reduce: str = 'sum'):
+                 reduction: str = 'sum'):
     """
     Computes the log-mse loss between `x` and `y` as defined in [1], eq. 11.
     The `reduction` only affects the speaker dimension; the time dimension is always
@@ -49,16 +49,23 @@ def log_mse_loss(estimate: torch.Tensor, target: torch.Tensor,
             TasNet: A Dissecting Approach.” ArXiv:1911.08895
             [Cs, Eess], November 20, 2019.
             http://arxiv.org/abs/1911.08895.
+
+    >>> estimate = [[1., 2, 3], [4, 5, 6]]
+    >>> target = [[2., 3, 4], [4, 0, 6]]
+    >>> log_mse_loss(torch.tensor(estimate), torch.tensor(target))
+    tensor(0.9208)
+    >>> log_mse_loss(torch.tensor(estimate), torch.tensor(target), reduction=None)
+    tensor([0.0000, 0.9208])
     """
     # Use the PyTorch implementation for MSE, should be the fastest
     return _reduce(
-        F.mse_loss(estimate, target, reduce='none').mean(dim=-1).log10(),
-        reduce=reduce
+        F.mse_loss(estimate, target, reduction='none').mean(dim=-1).log10(),
+        reduction=reduction
     )
 
 
 def sdr_loss(estimates: torch.Tensor, targets: torch.Tensor,
-             reduce: str = 'mean'):
+             reduction: str = 'mean', axis=-1):
     """
     The (scale dependent) SDR or SNR loss.
 
@@ -68,17 +75,24 @@ def sdr_loss(estimates: torch.Tensor, targets: torch.Tensor,
 
     Returns:
 
+    >>> estimate = [[1., 2, 3], [4, 5, 6]]
+    >>> target = [[2., 3, 4], [4, 0, 6]]
+    >>> sdr_loss(torch.tensor(estimate), torch.tensor(target))
+    tensor(-6.5167)
+    >>> sdr_loss(torch.tensor(estimate), torch.tensor(target), reduction=None)
+    tensor([-9.8528, -3.1806])
+
     """
     # Calculate the SNR. The square in the power computation is moved to the
     # front, thus the 20 in front of the log
     snr = 20 * torch.log10(
-        torch.norm(targets, dim=-1) / torch.norm(estimates - targets, dim=-1)
+        torch.norm(targets, dim=axis) / torch.norm(estimates - targets, dim=axis)
     )
 
-    return -_reduce(snr, reduce=reduce)
+    return -_reduce(snr, reduction=reduction)
 
 
-def si_sdr_loss(estimates, targets, reduce='mean', offset_invariant=False,
+def si_sdr_loss(estimates, targets, reduction='mean', offset_invariant=False,
                 grad_stop=False):
     """
     Scale Invariant SDR (SI-SDR) or Scale Invariant SNR (SI-SNR) loss as defined in [1], section 2.2.4.
@@ -96,6 +110,67 @@ def si_sdr_loss(estimates, targets, reduce='mean', offset_invariant=False,
     References:
         [1] TASNET: TIME-DOMAIN AUDIO SEPARATION NETWORK FOR REAL-TIME,
             SINGLE-CHANNEL SPEECH SEPARATION
+
+    >>> estimate = [[1., 2, 3], [4, 5, 6]]
+    >>> target = [[2., 3, 4], [4, 0, 6]]
+    >>> si_sdr_loss(torch.tensor(estimate), torch.tensor(target))
+    tensor(-10.7099)
+    >>> si_sdr_loss(torch.tensor(estimate), torch.tensor(target), reduction=None)
+    tensor([-18.2391,  -3.1806])
+
+    >>> import numpy as np
+    >>> from pb_bss.evaluation import si_sdr
+    >>> np.random.seed(0)
+    >>> reference = torch.tensor(np.random.randn(100))
+
+    >>> def calculate(estimates, targets):
+    ...     print('Torch loss:', si_sdr_loss(estimates, targets))
+    ...     print('Numpy metric:', si_sdr(estimates.numpy(), targets.numpy()))
+
+    Perfect estimation
+    >>> calculate(reference, reference)
+    Torch loss: tensor(-313.2011, dtype=torch.float64)
+    Numpy metric: inf
+    >>> si_sdr_loss(reference.to(torch.float32), reference.to(torch.float32))
+    tensor(-141.5990)
+    >>> calculate(reference, reference * 2)
+    Torch loss: tensor(-313.2011, dtype=torch.float64)
+    Numpy metric: inf
+
+    >>> calculate(reference, torch.flip(reference, (-1,)))
+    Torch loss: tensor(25.1277, dtype=torch.float64)
+    Numpy metric: -25.127672346460717
+    >>> calculate(reference, reference + torch.flip(reference, (-1,)))
+    Torch loss: tensor(-0.4811, dtype=torch.float64)
+    Numpy metric: 0.481070445785553
+    >>> calculate(reference, reference + 0.5)
+    Torch loss: tensor(-6.3705, dtype=torch.float64)
+    Numpy metric: 6.3704606032577304
+    >>> calculate(reference, reference * 2 + 1)
+    Torch loss: tensor(-6.3705, dtype=torch.float64)
+    Numpy metric: 6.3704606032577304
+    >>> calculate(
+    ...    torch.tensor([1., 0], dtype=torch.float64),
+    ...    torch.tensor([0., 0], dtype=torch.float64))  # never predict only zeros nan
+    Torch loss: tensor(nan, dtype=torch.float64)
+    Numpy metric: nan
+    >>> calculate(
+    ...     torch.tensor([reference.numpy(), reference.numpy()]),
+    ...     torch.tensor([reference.numpy() * 2 + 1, reference.numpy() * 1 + 0.5])
+    ... )
+    Torch loss: tensor(-6.3705, dtype=torch.float64)
+    Numpy metric: [6.3704606 6.3704606]
+
+    >>> calculate(
+    ...     torch.tensor([0., 0], dtype=torch.float64),
+    ...     torch.tensor([0., 0], dtype=torch.float64))  # never predict only zeros
+    Torch loss: tensor(nan, dtype=torch.float64)
+    Numpy metric: nan
+    >>> calculate(
+    ...     torch.tensor([0., 0], dtype=torch.float64),
+    ...     torch.tensor([1., 0], dtype=torch.float64))  # never predict only zeros
+    Torch loss: tensor(nan, dtype=torch.float64)
+    Numpy metric: nan
     """
     assert estimates.shape == targets.shape, (estimates.shape, targets.shape)
     assert len(estimates.shape) >= 1, estimates.shape
@@ -118,11 +193,11 @@ def si_sdr_loss(estimates, targets, reduce='mean', offset_invariant=False,
 
     # The SNR loss computes e_noise ([1] eq. 14) and the ratio, here the
     # SI-SNR ([1] eq. 15)
-    return sdr_loss(estimates, s_target, reduce=reduce)
+    return sdr_loss(estimates, s_target, reduction=reduction)
 
 
 def log1p_mse_loss(estimate: torch.Tensor, target: torch.Tensor,
-                   reduce: str = 'sum'):
+                   reduction: str = 'sum'):
     """
     Computes the log1p-mse loss between `x` and `y` as defined in [1], eq. 4.
     The `reduction` only affects the speaker dimension; the time dimension is
@@ -152,12 +227,19 @@ def log1p_mse_loss(estimate: torch.Tensor, target: torch.Tensor,
             Haeb-Umbach. „Multi-talker ASR for an unknown number of sources:
             Joint training of source counting, separation and ASR“.
             http://arxiv.org/abs/2006.02786.
+
+    >>> estimate = [[1., 2, 3], [4, 5, 6]]
+    >>> target = [[2., 3, 4], [4, 0, 6]]
+    >>> log1p_mse_loss(torch.tensor(estimate), torch.tensor(target))
+    tensor(1.2711)
+    >>> log1p_mse_loss(torch.tensor(estimate), torch.tensor(target), reduction=None)
+    tensor([0.3010, 0.9700])
     """
     # Use the PyTorch implementation for MSE, should be the fastest
     return _reduce(
         torch.log10(
-            1 + F.mse_loss(estimate, target, reduce='none').mean(dim=-1)),
-        reduce=reduce
+            1 + F.mse_loss(estimate, target, reduction='none').mean(dim=-1)),
+        reduction=reduction
     )
 
 # TODO: Add log1p_mse loss from interspeech paper
