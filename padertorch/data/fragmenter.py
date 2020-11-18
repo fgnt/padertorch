9b21634467b58d3d4593667cccd7cc57c6425169
jensheit <heitkaemper@nt.upb.de>
2020-11-18 11:50:56 +0100

    adds comments and changes some variable names after discussion

--- padertorch/data/fragmenter.py
+++ padertorch/data/fragmenter.py
@@ -3,6 +3,16 @@ import numpy as np
 from paderbox.utils.nested import nested_op, flatten, deflatten
 from paderbox.array import segment_axis
 
+
+possible_boundary_modes = [
+    'begin',
+    'end',
+    'center',
+    'random',
+    'random_max_segments',
+]
+
+
 class Fragmenter(object):
     """
     Build fragments of the values corresponding to fragment_keys 
@@ -109,86 +119,145 @@ class Fragmenter(object):
         return fragments
 
 
-
-def get_segment_offsets(num_samples, length, shift=None,
-                        offset_mode='start', num_segments=None):
+def get_segment_boundaries(num_samples, length, shift=None,
+                           mode='begin', choose_one=False, rng=np.random):
     """
 
     Args:
-        num_samples:
-        length:
-        shift:
-        offset_mode:
-        num_segments:
+        num_samples: num samples of signal for which boundaries are caclulated
+        length: segment length
+        shift: shift between segments
+        mode: Defines the position of the boundaries in the signal:
+            begin: boundaries start at sample zero
+                so that only values at the end are cut
+            end: boundaries end at num_samples
+                so that only values at the beginning are cut
+            center: boundaries are chosen such that the same number of samples
+                are discarded at the end and the beginning of the signal
+            random: Starts the first segment randomly at a value
+                between 0 and shift. This may reduce the number of segments.
+            random_max_segments: Randomly chooses the first segment such that
+                the maximum number of segments are created
+        choose_one: If True only outputs one segment boundary
+        rng: random number generator (numpy.random)
 
     Returns:
-    >>> get_segment_offsets(25, 10, None, offset_mode='start')
-    array([ 0, 10])
-    >>> get_segment_offsets(25, 10, 3, offset_mode='start')
-    array([ 0,  3,  6,  9, 12, 15])
-    >>> get_segment_offsets(24, 10, 3, offset_mode='start')
-    array([ 0,  3,  6,  9, 12])
-    >>> get_segment_offsets(24, 10, 3, offset_mode='end')
-    array([ 2,  5,  8, 11, 14])
-    >>> get_segment_offsets(24, 10, 3, offset_mode='center')
-    array([ 1,  4,  7, 10, 13])
-    >>> get_segment_offsets(24, 10, 3, offset_mode='center', num_segments=2)
-    array([1, 4])
-    >>> get_segment_offsets(24, 10, 3, offset_mode='center', num_segments=1)
-    array([1])
+        2xB numpy array with start and end values for B boundaries
+
+    >>> np.random.seed(0)
+    >>> get_segment_boundaries(25, 10, None, mode='begin')
+    array([[ 0, 10],
+           [10, 20]])
+    >>> get_segment_boundaries(25, 10, 3, mode='begin')
+    array([[ 0,  3,  6,  9, 12, 15],
+           [10, 13, 16, 19, 22, 25]])
+    >>> get_segment_boundaries(24, 10, 3, mode='begin')
+    array([[ 0,  3,  6,  9, 12],
+           [10, 13, 16, 19, 22]])
+    >>> get_segment_boundaries(24, 10, 3, mode='random')
+    array([[ 0,  3,  6,  9, 12],
+           [10, 13, 16, 19, 22]])
+    >>> get_segment_boundaries(24, 10, 3, mode='random_max_segments')
+    array([[ 1,  4,  7, 10, 13],
+           [11, 14, 17, 20, 23]])
+    >>> get_segment_boundaries(24, 10, 3, mode='center')
+    array([[ 1,  4,  7, 10, 13],
+           [11, 14, 17, 20, 23]])
+    >>> get_segment_boundaries(24, 10, 3, mode='center', choose_one=True)
+    array([[ 1],
+           [11]])
     """
     assert num_samples >= length, (num_samples, length)
     if shift is None:
         shift = length
     assert shift > 0, shift
 
-    if offset_mode == 'start':
+    if mode == 'begin':
         start = 0
-    elif offset_mode == 'random':
-        start = np.random.randint(num_samples - length)
-    elif offset_mode in ['end', 'center']:
+    elif mode == 'random':
+        start = rng.randint(shift)
+    elif mode == 'random_max_segments':
+        start = rng.randint((num_samples - length) % shift)
+    elif mode in ['end', 'center']:
         remainder = (num_samples - length) % shift
-        if offset_mode == 'end':
+        if mode == 'end':
             start = remainder
         else:
             start = remainder // 2
     else:
-        raise ValueError('Unknown offset mode', offset_mode)
-    offset = np.arange(start, num_samples - length + 1, shift)
+        raise ValueError('Unknown offset mode', mode,
+                         'choose on of', possible_boundary_modes)
+    start = np.arange(start, num_samples - length + 1, shift)
+    if choose_one:
+        start = rng.choice(start, 1)
+    stop = start + length
+    boundaries = np.stack([start, stop], axis=0)
+    return boundaries
 
-    if num_segments:
-        max_segments = (num_samples - length) // shift + 1
-        assert num_segments <= max_segments, (num_segments, max_segments)
-        offset = offset[:num_segments]
-    return offset
 
-
-def segment(x, length, offset=None, shift=None, axis=-1,
-            offset_mode='start', num_segments=None):
+def segment(x, axis=-1, boundary=None, length: int=None, shift=None,
+            mode='begin', choose_one=False, rng=np.random):
     """
+    Segments a signal x along an axis. Either with predefined segment
+    boundaries if boundary is set or with internally calculated boundaries if
+    length is set.
 
     Args:
-        x:
-        segment_length:
-        step:
-        axis:
-        offset_mode:
-        num_segments:
+        x: signal to be segmented
+        axis: axis which is segmented
+        boundary: 2xB numpy array with start and end values
+            for B pre calculated boundaries, which may be used to segment
+            multiple audios in the same way.
+        length: segment length
+        shift: shift between segments
+        mode: Defines the position of the boundaries in the signal:
+            begin: boundaries start at sample zero
+                so that only values at the end are cut
+            end: boundaries end at num_samples
+                so that only values at the beginning are cut
+            center: boundaries are chosen such that the same number of samples
+                are discarded at the end and the beginning of the signal
+            random: Starts the first segment randomly at a value
+                between 0 and shift
+            random_max_segments: Randomly chooses the first segment such that
+                the maximum number of segments are created
+        choose_one: If True only outputs one segment boundary
+        rng: random number generator (numpy.random)
 
     Returns:
 
-    >>> segment(np.arange(0, 15), 10, None, 3, -1)
+    >>> segment(np.arange(0, 15), -1, None, 10, 3)
     array([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
            [ 3,  4,  5,  6,  7,  8,  9, 10, 11, 12]])
     """
 
-    if offset is None:
-        offset = get_segment_offsets(x.shape[axis], length, shift,
-                                      offset_mode, num_segments)
+    if boundary is None:
+        boundary = get_segment_boundaries(x.shape[axis], length, shift,
+                                            mode, choose_one, rng)
+    else:
+        assert length is None, (
+            'Either use predefined boundaries or set a segment length'
+            'because only one of these information can be used.'
+        )
+        if isinstance(boundary, (list, tuple)):
+            boundary = np.array(boundary)
+            if boundary.ndim == 1:
+                boundary = boundary[..., None]
+            assert boundary.ndim == 2, boundary.shape
+        elif x.__class__.__module__ == 'numpy':
+            assert x.ndim == 2, x.shape
+        elif x.__class__.__module__ == 'torch':
+            assert x.dim() == 2, x.shape
+        else:
+            raise TypeError('The signal x has an unknown type', type(x))
+
+        assert boundary.shape[0] == 2, 'The first dimension has to be 2 to' \
+                                       ' describe start and stop of a segment'
+
     # slice the array to remove samples not addressed with
     # this offsets-segment_length combination
     slc = [slice(None)] * x.ndim
-    slc[axis] = slice(offset[0], offset[-1] + length)
+    slc[axis] = slice(boundary[0, 0], boundary[1, -1])
     x = x[tuple(slc)]
 
     return segment_axis(x, length, shift, end='cut')
