7c7feab217457bdf3f150dca069b6edd5a2d45d5
jensheit <heitkaemper@nt.upb.de>
2020-11-25 15:41:34 +0100

    update test_cases

--- tests/test_data/test_segmenter.py
+++ tests/test_data/test_segmenter.py
@@ -143,8 +143,7 @@ def test_include_exclude():
         assert all([key in entry.keys() for key in ex.keys()])
         np.testing.assert_equal(
             entry['x'], np.arange(idx * 16000, 16000 + (idx + 1) * 16000))
-        np.testing.assert_equal(entry['audio_data']['y'],
-                                np.arange(65000)[:, None])
+        np.testing.assert_equal(entry['y'], np.arange(65000))
 
 
 def test_axis():
@@ -192,8 +191,8 @@ def test_axis_dict_wildcard():
         )
         np.testing.assert_equal(entry['audio_data']['x'],
                                 entry['audio_data']['y'])
-        np.testing.assert_equal(entry['audio_data']['z'],
-                                np.arange(65000)[:, None])
+        np.testing.assert_equal(entry['z'],
+                                np.arange(65000))
 
 
 def test_wildcard():
