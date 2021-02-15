890a057806c9a48401458bd8b5f32cead19a9ce8
Thilo von Neumann <tvn@mail.upb.de>
2021-02-15 07:12:13 +0100

    Print warning when mask_to_image clips

--- padertorch/summary/tbx_utils.py
+++ padertorch/summary/tbx_utils.py
@@ -68,6 +68,10 @@ def mask_to_image(
 
     For more details of the output shape, see the tensorboardx docs
 
+    Note:
+        Clips mask to range [0, 1]. Any values outside of this range will be
+        ignored.
+
     Args:
         mask: Mask to plot
         batch_first: If `True`, `signal` is expected to have shape
@@ -83,6 +87,13 @@ def mask_to_image(
         Colorized image with shape (color (1 or 3), features, frames)
     """
     mask = to_numpy(mask, detach=True)
+
+    if np.any(mask < 0) or np.any(mask > 1):
+        import warning
+        warning.warn(
+            f'Mask value passed to mask_to_image out of range ([0, 1])! '
+            f'Clipping to range!'
+        )
 
     image = np.clip(mask * 255, 0, 255)
     image = image.astype(np.uint8)
