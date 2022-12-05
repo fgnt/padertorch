b7ba24a42a05745d127a74a519af08a876319a95
Janek Ebbers <ebbers@nt.upb.de>
2022-12-05 13:32:34 +0100

    update RNN / Transformer Modules in contrib.je

--- padertorch/contrib/je/modules/rnn.py
+++ padertorch/contrib/je/modules/rnn.py
@@ -4,63 +4,127 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from padertorch.ops.sequence.mask import compute_mask
 from einops import rearrange
 from padertorch.contrib.je.modules.conv import CNN1d, CNNTranspose1d
+from padertorch.contrib.je.modules.transformer import TransformerLayerStack
 
 
 class RNN(nn.Module):
-    rnn_cls = None
-
-    def __init__(
-            self, input_size, hidden_size, num_layers=1,
-            bias=True, dropout=0., bidirectional=False, reverse=False,
-            output_net=None,
-    ):
+    def __init__(self, rnn, output_net=None, reverse=False):
         super().__init__()
-        self.rnn = self.rnn_cls(
-            input_size=input_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            bias=bias,
-            batch_first=True,
-            dropout=dropout,
-            bidirectional=bidirectional,
-        )
-        self.reverse = reverse
+        # self.rnn = self.rnn_cls(
+        #     input_size=input_size,
+        #     hidden_size=hidden_size,
+        #     num_layers=num_layers,
+        #     bias=bias,
+        #     batch_first=True,
+        #     dropout=dropout,
+        #     bidirectional=bidirectional,
+        # )
+        self.rnn = rnn
         self.output_net = output_net
+        self.reverse = reverse
 
     def forward(self, x, seq_len):
-        x = rearrange(x, 'b f t -> b t f')
-        if self.reverse:
-            x = reverse_sequence(x, seq_len=seq_len)
-        if seq_len is not None:
-            x = pack_padded_sequence(
-                x, seq_len, batch_first=self.rnn.batch_first
-            )
-        x, _ = self.rnn(x)
-        if seq_len is not None:
-            x = pad_packed_sequence(x, batch_first=self.rnn.batch_first)[0]
-        if self.reverse:
-            x = reverse_sequence(x, seq_len=seq_len)
-        x = rearrange(x, 'b t f -> b f t')
+        if self.rnn is not None:
+            if isinstance(self.rnn, nn.RNNBase):
+                self.rnn.flatten_parameters()
+            x = rearrange(x, 'b f t -> b t f')
+            if self.reverse:
+                x = reverse_sequence(x, seq_len=seq_len)
+            if isinstance(self.rnn, TransformerLayerStack):
+                x, _ = self.rnn(x, seq_len)
+            else:
+                assert self.rnn.batch_first is True, self.rnn.batch_first
+                if seq_len is not None:
+                    x = pack_padded_sequence(
+                        x, seq_len, batch_first=self.rnn.batch_first
+                    )
+                x, _ = self.rnn(x)
+                if seq_len is not None:
+                    x = pad_packed_sequence(x, batch_first=self.rnn.batch_first)[0]
+            if self.reverse:
+                x = reverse_sequence(x, seq_len=seq_len)
+            x = rearrange(x, 'b t f -> b f t')
         if self.output_net is not None:
             x, seq_len = self.output_net(x, seq_len)
         return x, seq_len
+
+    def freeze(self, num_layers=None):
+        # ToDo: does this method work with all RNN types inkl. Transformer?
+        if num_layers == 0:
+            return
+        num_rnn_layers = self.rnn.num_layers if num_layers is None else min(num_layers, self.rnn.num_layers)
+        for name, param in self.rnn.named_parameters():
+            name_split = name.split('.')
+            if name.endswith(f'_l{num_rnn_layers}') or (
+                (len(name_split) > 1) and (name_split[1] == str(num_rnn_layers))
+            ):
+                break
+            print(f'Freeze {name}')
+            param.requires_grad = False
+        num_out_layers = None if num_layers is None else max(num_layers - self.rnn.num_layers, 0)
+        if self.output_net is not None:
+            print(f'Freeze {num_out_layers} output_net layers')
+            self.output_net.freeze(num_out_layers)
 
     @classmethod
     def finalize_dogmatic_config(cls, config):
         if config['output_net'] is not None:
             config['output_net']['factory'] = CNN1d
             if config['output_net']['factory'] in [CNN1d, CNNTranspose1d]:
-                config['output_net']['in_channels'] = config['hidden_size'] * (1 + config['bidirectional'])
+                if config['rnn'] is not None:
+                    config['output_net']['in_channels'] = config['rnn']['hidden_size'] * (1 + config['rnn']['bidirectional'])
             else:
                 raise ValueError(f'output_net factory {config["output_net"]["factory"]} not allowed.')
 
 
 class GRU(RNN):
-    rnn_cls = nn.GRU
+    @classmethod
+    def finalize_dogmatic_config(cls, config):
+        config['rnn'] = {
+            'factory': nn.GRU,
+            'num_layers': 1,
+            'bias': True,
+            'dropout': 0.,
+            'bidirectional': False,
+            'batch_first': True,
+        }
+        assert config['rnn'] is None or config['rnn']['factory'] == nn.GRU, config['rnn']
+        super().finalize_dogmatic_config(config)
 
 
 class LSTM(RNN):
-    rnn_cls = nn.LSTM
+    @classmethod
+    def finalize_dogmatic_config(cls, config):
+        config['rnn'] = {
+            'factory': nn.LSTM,
+            'num_layers': 1,
+            'bias': True,
+            'dropout': 0.,
+            'bidirectional': False,
+            'batch_first': True,
+        }
+        assert config['rnn'] is None or config['rnn']['factory'] == nn.LSTM, config['rnn']
+        super().finalize_dogmatic_config(config)
+
+
+class TransformerEncoder(RNN):
+    @classmethod
+    def finalize_dogmatic_config(cls, config):
+        config['rnn'] = {
+            'factory': TransformerLayerStack,
+            'num_layers': 6,
+            'hidden_size': 512,
+            'num_heads': 8,
+            'd_ff': 2048,
+            'bidirectional': False,
+            'cross_attention': False,
+            'activation_ff': 'relu',
+            'dropout': 0.,
+            'positional_encoding': True,
+        }
+        assert config['rnn'] is None or config['rnn']['factory'] == TransformerLayerStack, config['rnn']
+        assert config['rnn']['cross_attention'] is False, config['rnn']['cross_attention']
+        super().finalize_dogmatic_config(config)
 
 
 def reverse_sequence(x, seq_len=None):
--- padertorch/contrib/je/modules/transformer.py
+++ padertorch/contrib/je/modules/transformer.py
@@ -7,8 +7,6 @@ from padertorch.base import Module
 from padertorch.ops.mappings import ACTIVATION_FN_MAP
 from padertorch.modules.normalization import Normalization
 from padertorch.ops.sequence.mask import compute_mask
-from padertorch.contrib.je.modules.rnn import reverse_sequence
-from padertorch.contrib.je.modules.conv import CNN1d, CNNTranspose1d
 
 
 def scaled_dot_product_attention(q, k, v, seq_len=None, bidirectional=False, mask=None):
@@ -57,57 +55,57 @@ class MultiHeadAttention(Module):
     torch.Size([2, 3, 4])
     """
     def __init__(
-            self, queue_size, key_size, value_size, hidden_size, output_size, num_heads=1, bidirectional=False
+            self, queue_size, key_size, value_size, d_model, output_size,
+            num_heads=8, bidirectional=False
     ):
         super().__init__()
         self.queue_size = queue_size
-        self.hidden_size = hidden_size
+        self.d_model = d_model
         self.output_size = output_size
         self.num_heads = num_heads
         self.bidirectional = bidirectional
-        self.lin_queue = torch.nn.Linear(queue_size, self.hidden_size)
-        self.lin_key = torch.nn.Linear(key_size, self.hidden_size)
-        self.lin_value = torch.nn.Linear(value_size, self.hidden_size)
-        self.out = torch.nn.Linear(self.hidden_size, self.output_size)
+        self.lin_queue = torch.nn.Linear(queue_size, self.d_model)
+        self.lin_key = torch.nn.Linear(key_size, self.d_model)
+        self.lin_value = torch.nn.Linear(value_size, self.d_model)
+        self.out = torch.nn.Linear(self.d_model, self.output_size)
 
     def forward(self, q, k, v, seq_len=None, mask=None):
         B, Tq, _ = q.shape
         B, Tk, _ = k.shape
         q = self.lin_queue(q).view(
-            B, Tq, self.num_heads, self.hidden_size//self.num_heads
+            B, Tq, self.num_heads, self.d_model//self.num_heads
         ).transpose(1, 2)
         k = self.lin_key(k).view(
-            B, Tk, self.num_heads, self.hidden_size//self.num_heads
+            B, Tk, self.num_heads, self.d_model//self.num_heads
         ).transpose(1, 2)
         v = self.lin_value(v).view(
-            B, Tk, self.num_heads, self.hidden_size//self.num_heads
+            B, Tk, self.num_heads, self.d_model//self.num_heads
         ).transpose(1, 2)
         x, attention_weights = scaled_dot_product_attention(
             q, k, v, seq_len=seq_len, bidirectional=self.bidirectional, mask=mask
         )
-        x = x.transpose(1, 2).contiguous().view(B, Tq, self.hidden_size)
+        x = x.transpose(1, 2).contiguous().view(B, Tq, self.d_model)
         return self.out(x), attention_weights
 
 
-class TransformerBlock(Module):
+class TransformerLayer(Module):
     """
     https://arxiv.org/abs/1706.03762
     """
     def __init__(
-            self, input_size, hidden_size, output_size, num_heads=1,
-            bidirectional=False, cross_attention=False,
-            norm='layer', norm_kwargs={}, activation='relu', dropout=.0,
+            self, d_model=512, d_ff=2048, num_heads=8,
+            bidirectional=True, cross_attention=False,
+            norm='layer', norm_kwargs={}, norm_first=True,
+            activation_ff='relu', dropout=.0,
     ):
         super().__init__()
-        self.activation = ACTIVATION_FN_MAP[activation]()
-        self.dropout = dropout
         self.multi_head_self_attention = MultiHeadAttention(
-            input_size, input_size, input_size, hidden_size, hidden_size,
+            d_model, d_model, d_model, d_model, d_model,
             num_heads=num_heads, bidirectional=bidirectional
         )
         self.cross_attention = cross_attention
-        self.hidden = torch.nn.Linear(hidden_size, hidden_size)
-        self.out = torch.nn.Linear(hidden_size, output_size)
+        self.hidden = torch.nn.Linear(d_model, d_ff)
+        self.out = torch.nn.Linear(d_ff, d_model)
 
         if norm is None:
             self.self_attention_norm = None
@@ -115,7 +113,7 @@ class TransformerBlock(Module):
         else:
             norm_kwargs = {
                 "data_format": 'btc',
-                "shape": (None, None, hidden_size),
+                "shape": (None, None, d_model),
                 'eps': 1e-2,
                 **norm_kwargs
             }
@@ -125,109 +123,113 @@ class TransformerBlock(Module):
                 norm_kwargs['statistics_axis'] = 'c'
             else:
                 raise ValueError(f'{norm} normalization not known.')
-            norm_kwargs["shape"] = (None, None, hidden_size)
             self.self_attention_norm = Normalization(**norm_kwargs)
-            norm_kwargs["shape"] = (None, None, output_size)
             self.output_norm = Normalization(**norm_kwargs)
 
         if cross_attention:
             self.multi_head_cross_attention = MultiHeadAttention(
-                hidden_size*num_heads, hidden_size*num_heads, hidden_size*num_heads,
-                hidden_size, hidden_size*num_heads,
-                num_heads, bidirectional=True
+                d_model, d_model, d_model, d_model, d_model,
+                num_heads=num_heads, bidirectional=True
             )
             if norm is None:
                 self.cross_attention_norm = None
             else:
-                norm_kwargs["shape"] = (None, None, hidden_size)
                 self.cross_attention_norm = Normalization(**norm_kwargs)
+        self.norm_first = norm_first
+        self.activation_ff = ACTIVATION_FN_MAP[activation_ff]()
+        self.dropout = dropout
 
-    def forward(self, x, seq_len, v=None, seq_len_v=None, state=None):
-        x_ = x if state is None else torch.cat([state, x], 1)
-        h, _ = self.multi_head_self_attention(x, x_, x_, seq_len=seq_len)
+    def forward(
+            self, x, seq_len, m=None, seq_len_m=None, state=None,
+    ):
+        if state is not None:
+            assert self.multi_head_self_attention.bidirectional is False
+        s = x if state is None else torch.cat((state, x), 1)
+        h, _ = self.multi_head_self_attention(x, s, s, seq_len=seq_len)
         if self.training and self.dropout > 0.:
             h = F.dropout(h, self.dropout)
-        if h.shape == x.shape:
-            h = h + x
-        if self.self_attention_norm is not None:
+        if self.self_attention_norm is not None and self.norm_first:
+            h = self.self_attention_norm(h, sequence_lengths=seq_len)
+        h = h + x
+        if self.self_attention_norm is not None and not self.norm_first:
             h = self.self_attention_norm(h, sequence_lengths=seq_len)
         if self.cross_attention:
-            assert v is not None
+            assert m is not None
             q = h
-            h, _ = self.multi_head_cross_attention(q, v, v, seq_len=seq_len_v)
+            h, _ = self.multi_head_cross_attention(q, m, m, seq_len=seq_len_m)
             if self.training and self.dropout > 0.:
                 h = F.dropout(h, self.dropout)
-            if h.shape == q.shape:
-                h = h + q
-            if self.cross_attention_norm is not None:
+            if self.cross_attention_norm is not None and self.norm_first:
                 h = self.cross_attention_norm(h, sequence_lengths=seq_len)
-        y = self.out(self.activation(self.hidden(h)))
+            h = h + q
+            if self.cross_attention_norm is not None and not self.norm_first:
+                h = self.cross_attention_norm(h, sequence_lengths=seq_len)
+        y = self.out(self.activation_ff(self.hidden(h)))
         if self.training and self.dropout > 0.:
             y = F.dropout(y, self.dropout)
-        if h.shape == y.shape:
-            y = y + h
-        if self.output_norm is not None:
+        if self.output_norm is not None and self.norm_first:
             y = self.output_norm(y, sequence_lengths=seq_len)
-        return y, x_
+        y = y + h
+        if self.output_norm is not None and not self.norm_first:
+            y = self.output_norm(y, sequence_lengths=seq_len)
+        return y, s
 
 
-class TransformerStack(Module):
+class TransformerLayerStack(Module):
     def __init__(
-            self, input_size, hidden_size, output_net=None, num_heads=1,
-            num_layers=1, bidirectional=False, reverse=False, cross_attention=False,
-            activation='relu', norm='layer', norm_kwargs={},
-            positional_encoding=True, dropout=0.,
-            return_state=False,
+            self, input_size, hidden_size=512, d_ff=2048, num_heads=8,
+            num_layers=6, bidirectional=False, cross_attention=False,
+            norm='layer', norm_kwargs={}, norm_first=True,
+            activation_ff='relu', dropout=0., positional_encoding=True,
     ):
         """
         https://arxiv.org/abs/1706.03762
 
         Args:
             input_size:
-            hidden_size:
-            output_size:
+            hidden_size: d_model
+            d_ff:
             num_heads:
             bidirectional:
             cross_attention:
-            activation:
             norm:
             norm_kwargs:
+            norm_first:
+            activation_ff:
+            dropout:
+            positional_encoding:
 
         Returns:
 
         >>> x = torch.zeros((2, 3, 8))
-        >>> attn = TransformerStack(8, 6, 6, 1, 2, bidirectional=True)
-        >>> attn(x, seq_len=[1, 2]).shape
+        >>> attn = TransformerLayerStack(8, 6, 20, 1, 2, bidirectional=True)
+        >>> attn(x, seq_len=[1, 2])[0].shape
         torch.Size([2, 3, 6])
-        >>> attn = TransformerStack(8, 6, 6, 2, 2, bidirectional=True)
-        >>> attn(x, seq_len=[1, 2]).shape
+        >>> attn = TransformerLayerStack(8, 6, 20, 2, 2, bidirectional=True)
+        >>> attn(x, seq_len=[1, 2])[0].shape
         torch.Size([2, 3, 6])
-        >>> attn(x, seq_len=None, state=[torch.zeros((2, 6, 8)), torch.zeros((2, 6, 6))]).shape
+        >>> attn = TransformerLayerStack(8, 6, 20, 2, 2, bidirectional=False)
+        >>> attn(x, seq_len=None)[0].shape
         torch.Size([2, 3, 6])
-        >>> attn = TransformerStack(8, 6, 6, 2, 2, bidirectional=False)
-        >>> attn(x, seq_len=None).shape
-        torch.Size([2, 3, 6])
-        >>> attn(x, seq_len=None, state=[torch.zeros((2, 6, 8)), torch.zeros((2, 6, 6))]).shape
+        >>> attn(x, seq_len=None, state=[torch.zeros((2, 5, 6)), torch.zeros((2, 5, 6))])[0].shape
         torch.Size([2, 3, 6])
         """
         super().__init__()
-        self.reverse = reverse
         self.positional_encoding = positional_encoding
         self.dropout = dropout
-        self.return_state = return_state
-        stack = list()
+        self.num_layers = num_layers
+        self.lin = torch.nn.Linear(input_size, hidden_size)
+        transformer_layers = list()
         for i in range(num_layers):
-            stack.append(
-                TransformerBlock(
-                    input_size, hidden_size, hidden_size, num_heads,
-                    bidirectional=bidirectional, cross_attention=cross_attention,
-                    activation=activation, norm=norm, norm_kwargs=norm_kwargs,
-                    dropout=dropout,
+            transformer_layers.append(
+                TransformerLayer(
+                    hidden_size, d_ff, num_heads, bidirectional=bidirectional,
+                    cross_attention=cross_attention,
+                    norm=norm, norm_kwargs=norm_kwargs, norm_first=norm_first,
+                    activation_ff=activation_ff, dropout=dropout,
                 )
             )
-            input_size = hidden_size
-        self.stack = torch.nn.ModuleList(stack)
-        self.output_net = output_net
+        self.transformer_layers = torch.nn.ModuleList(transformer_layers)
 
     def add_positional_encoding(self, x):
         b, t, d = x.shape
@@ -240,38 +242,18 @@ class TransformerStack(Module):
         pos_encodings = rearrange(pos_encodings, 't d1 d2 -> t (d1 d2)')
         return x + pos_encodings
 
-    def forward(self, x, seq_len, v=None, seq_len_v=None, state=None):
-        x = rearrange(x, 'b f t -> b t f')
-        if self.reverse:
-            x = reverse_sequence(x, seq_len=seq_len)
-        new_state = []
+    def forward(self, x, seq_len, m=None, seq_len_m=None, state=None):
+        h = self.lin(x)
         if self.positional_encoding:
-            x = self.add_positional_encoding(x)
-        if self.training and self.dropout > 0.:
-            x = F.dropout(x, self.dropout)
-        for i, layer in enumerate(self.stack):
-            x, x_ = layer(
-                x, seq_len=seq_len, v=v, seq_len_v=seq_len_v,
-                state=None if state is None else state[i],
+            h = self.add_positional_encoding(h)
+        if state is None:
+            state = len(self.transformer_layers) * [None]
+        for i, layer in enumerate(self.transformer_layers):
+            h, state[i] = layer(
+                h, seq_len=seq_len, m=m, seq_len_m=seq_len_m,
+                state=state[i],
             )
-            new_state.append(x_)
-        if self.reverse:
-            x = reverse_sequence(x, seq_len=seq_len)
-        x = rearrange(x, 'b t f -> b f t')
-        if self.output_net is not None:
-            x, seq_len = self.output_net(x, seq_len)
-        if self.return_state:
-            return x, seq_len, new_state
-        return x, seq_len
-
-    @classmethod
-    def finalize_dogmatic_config(cls, config):
-        if config['output_net'] is not None:
-            config['output_net']['factory'] = CNN1d
-            if config['output_net']['factory'] in [CNN1d, CNNTranspose1d]:
-                config['output_net']['in_channels'] = config['hidden_size']
-            else:
-                raise ValueError(f'output_net factory {config["output_net"]["factory"]} not allowed.')
+        return h, state
 
 
 def get_causal_mask(x):
