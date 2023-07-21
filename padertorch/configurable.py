cf17a379b41e47c20f2bac3262a0a46518bbdc6c
Michael Kuhlmann <kuhlmann@nt.uni-paderborn.de>
2023-07-21 18:16:59 +0200

    Replace deprecated collections.Mapping with collections.abc.Mapping
    collections.Mapping is deprecated since Python 3.3 and stops working with 3.10

--- padertorch/configurable.py
+++ padertorch/configurable.py
@@ -1703,7 +1703,7 @@ class _DogmaticConfig:
           'partial': 'torch.nn.Linear'},
          'storage_dir': 'abc'}
         """
-        if isinstance(dictionary, collections.Mapping):
+        if isinstance(dictionary, collections.abc.Mapping):
             special_key = _get_special_key(dictionary)
             if special_key:
                 dictionary[special_key] = cls._force_factory_type(
