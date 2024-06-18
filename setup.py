f6e5cb853a85fed41b225f8e5126b0dbfa74e705
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2024-06-18 13:50:39 +0000

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.0.1',
 
     description='A collection of common functionality to simplify the design, '
     'training and evaluation of machine learning models based on pytorch '
