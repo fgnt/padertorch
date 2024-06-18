60c5fc82997ac3e0f3dc2df463d74084119c8b5d
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2024-06-18 13:13:48 +0200

    prepare for pypi

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.10',
     ],
 
     # What does your project relate to?
@@ -102,7 +102,7 @@ setup(
         'natsort',
         'lazy_dataset',
         'IPython',
-        'paderbox @ git+http://github.com/fgnt/paderbox',
+        'paderbox',
     ],
 
     # Installation problems in a clean, new environment:
